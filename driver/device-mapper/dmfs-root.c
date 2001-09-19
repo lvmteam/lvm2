6371de51da1a9fdc5b132b21c71b754cc5067ab5
Steven Whitehouse <swhiteho@fedoraproject.org>
2001-09-19 21:27:46 +0000

    o Further changes to new file system interface

--- driver/device-mapper/dmfs-root.c
+++ driver/device-mapper/dmfs-root.c
@@ -41,6 +41,9 @@ static int dmfs_root_mkdir(struct inode *dir, struct dentry *dentry, int mode)
 	struct inode *inode;
 	struct mapped_device *md;
 
+	if (dentry->d_name.len >= DM_NAME_LEN)
+		return -EINVAL;
+
 	if (!is_identifier(name, dentry->d_name.len))
 		return -EPERM;
 
@@ -60,9 +63,15 @@ static int dmfs_root_mkdir(struct inode *dir, struct dentry *dentry, int mode)
 	return PTR_ERR(inode);
 }
 
+/*
+ * if u.generic_ip is not NULL, then it indicates an inode which
+ * represents a table. If it is NULL then the inode is a virtual
+ * file and should be deleted along with the directory.
+ */
 static inline positive(struct dentry *dentry)
 {
-	return dentry->d_inode && !d_unhashed(dentry);
+	return dentry->d_inode && dentry->d_inode->u.generic_ip &&
+	       !d_unhashed(dentry);
 }
 
 static int empty(struct dentry *dentry)
@@ -85,26 +94,52 @@ static int empty(struct dentry *dentry)
 	return 1;
 }
 
+static int dmfs_delete_virtual(struct inode *dir, struct dentry *dentry)
+{
+	struct list_head *list;
+	struct dentry *de;
+	int rv = 0;
+
+	while(1) {
+		spin_lock(&dcache_lock);
+		list = dentry->d_subdirs.next;
+		if (list == &dentry->d_subdirs) {
+			spin_unlock(&dcache_lock);
+			break;
+		}
+		de = list_entry(list, struct dentry, d_child);
+		if (de->d_inode && !d_unhashed(de)) {
+			spin_unlock(&dcache_lock);
+			if (de->d_inode->u.generic_ip)
+				BUG();
+			rv = de->d_inode->ops->unlink(dir, de);
+			if (rv == 0) {
+				d_delete(de);
+				continue;
+			}
+			break;
+		}
+		spin_unlock(&dcache_lock);
+	}
+
+	return rv;
+}
+
 static int dmfs_root_rmdir(struct inode *dir, struct dentry *dentry)
 {
 	int ret = -ENOTEMPTY;
 
-	/* 
-	 * FIXME: Checks need to be more complicated than this... 
-	 * Need to take into account removing "virtual" files but must
-	 * not allow removal whilst tables are still existing. Also we
-	 * should check for the block device being closed.
-	 */
-
 	if (empty(dentry)) {
 		struct inode *inode = dentry->d_inode;
 
-		inode->i_nlink--;
-		dm_remove(dentry->d_name.name);
-		dput(dentry);
+		ret = dmfs_delete_virtual(dir, dentry);
+		if (ret == 0) {
+			inode->i_nlink--;
+			dput(dentry);
+		}
 	}
 
-	return retval;
+	return ret;
 }
 
 static struct dentry *dmfs_root_lookup(struct inode *dir, struct dentry *dentry)
