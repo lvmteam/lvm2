dc1acd03f84636062fc0ab314a5c1e86dbd9a06a
Joe Thornber <thornber@redhat.com>
2001-09-28 13:08:44 +0000

    o generic hash table to store void *'s, not efficient, but adequate for LVM.

---
+++ lib/datastruct/hash.h
@@ -0,0 +1,26 @@
+/*
+ * tools/lib/datastruct/hash.h
+ *
+ * Copyright (C) 2001 Sistina Software
+ *
+ * This file is released under the GPL.
+ */
+
+#ifndef HASH_H
+#define HASH_H
+
+struct hash_table;
+typedef void (*iterate_fn)(void *data);
+
+struct hash_table *create_hash_table(unsigned size_hint);
+void destroy_hash_table(struct hash_table *t);
+
+char *hash_lookup(struct hash_table *t, const char *key);
+void hash_insert(struct hash_table *t, const char *key, void *data);
+void hash_remove(struct hash_table *t, const char *key);
+
+unsigned hash_get_num_entries(struct hash_table *t);
+void hash_iterate(struct hash_table *t, iterate_fn f);
+
+#endif
+
