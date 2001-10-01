852592066c0713a0a3171b5d5cbfd5e66042ece0
Alasdair Kergon <agk@redhat.com>
2001-10-01 19:29:52 +0000

    Misc structural changes.

---
+++ lib/formats/filter.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2001 Sistina Software
+ *
+ * lvm is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * lvm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU CC; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+#ifndef _LVM_FILTER_H
+#define _LVM_FILTER_H
+
+struct dev_filter *config_filter_create();
+
+void config_filter_destroy(struct dev_filter *f);
+
+#endif
+
