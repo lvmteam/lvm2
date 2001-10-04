a74c0f91ef69ef7d158d28e991ce85f828e0e947
Joe Thornber <thornber@redhat.com>
2001-10-04 11:40:13 +0000

    o define the uintN_t types

---
+++ lib/datastruct/lvm-types.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2001 Sistina Software (UK) Limited.
+ *
+ * This file is released under the GPL.
+ */
+
+#ifndef _LVM_TYPES_H
+#define _LVM_TYPES_H
+
+#include <sys/types.h>
+
+typedef __uint8_t uint8_t;
+typedef __uint16_t uint16_t;
+typedef __uint32_t uint32_t;
+typedef __uint64_t uint64_t;
+
+#if 0
+typedef __int8_t int8_t;
+typedef __int16_t int16_t;
+typedef __int32_t int32_t;
+typedef __int64_t int64_t;
+#endif
+
+#endif
