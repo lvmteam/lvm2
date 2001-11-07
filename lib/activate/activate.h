2bf8cc62cfa9beaf8a3c5a9ad10732e9576df309
Joe Thornber <joe@fib011235813.fsnet.co.uk>
2001-11-07 11:51:42 +0000

    o  Another pass at the activation code

--- lib/activate/activate.h
+++ lib/activate/activate.h
@@ -9,21 +9,32 @@
 
 /* FIXME Snapshot handling? */
 
+int lv_active(struct logical_volume *lv, int *result);
 int lv_activate(struct logical_volume *lv);
-
+int lv_reactivate(struct logical_volume *lv);
 int lv_deactivate(struct logical_volume *lv);
 
-/* Return number of LVs in the VG that are active */
+/*
+ * Return number of LVs in the VG that are
+ * active
+ */
 int lvs_in_vg_activated(struct volume_group *vg);
 
-/* Test for (lv->status & LVM_WRITE) */
+/*
+ * Test for (lv->status & LVM_WRITE)
+ */
 int lv_update_write_access(struct logical_volume *lv);
 
-/* Activate all LVs in the VG.  Ignore any that are already active. */
-/* Return number activated */
+/*
+ * Activate all LVs in the VG.  Ignore any that
+ * are already active.  Return number
+ * activated.
+ */
 int activate_lvs_in_vg(struct volume_group *vg);
 
-/* Deactivate all LVs in the VG */
+/*
+ * Deactivate all LVs in the VG
+ */
 int deactivate_lvs_in_vg(struct volume_group *vg);
 
 #endif
