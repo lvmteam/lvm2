1a5123da94f0ddca0ea92572047f9d94edf6a00d
Patrick Caulfield <pcaulfie@redhat.com>
2001-11-15 14:27:34 +0000

    define FMT_64 to be the right format string for 64-bit types a la GFS

--- lib/display/display.h
+++ lib/display/display.h
@@ -23,6 +23,14 @@
 
 #include "metadata.h"
 
+#include <stdint.h>
+
+#if (__WORDSIZE == 64)
+#define FMT_64 "l"
+#else
+#define FMT_64 "ll"
+#endif
+
 typedef	enum {SIZE_LONG=0, SIZE_SHORT=1} size_len_t;
 
 /* Specify size in KB */
