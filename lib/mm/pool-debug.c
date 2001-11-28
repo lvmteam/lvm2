4a8d12041577e2defb60dee009d2ca355ed35a9d
Joe Thornber <thornber@redhat.com>
2001-11-28 09:13:00 +0000

    o  pool-debug.c contains an alternative implementation of pool that gets    a seperate chunk of memory from dbg_malloc for each pool_alloc.  This    will allow the bounds checking code in dbg_malloc to do it's stuff.
    o  The normal implementation moved to pool-fast.c
    
    o  pool.c now just contains a #ifdef and includes the appropriate .c file.
    
    Alasdair, could you make sure that gcc -MM get's passed all the
    CFLAGS please, otherwise the dependencies get calculated incorrectly.

---
+++ lib/mm/pool-debug.c
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2001 Sistina Software (UK) Limited.
+ *
+ * This file is released under the LGPL.
+ */
+
+#include "pool.h"
+#include "dbg_malloc.h"
+#include "log.h"
+
+#include <assert.h>
+
+struct block {
+	struct block *next;
+	char data[0];
+};
+
+struct pool {
+	struct block *blocks;
+	struct block *tail;
+};
+
+/* by default things come out aligned for doubles */
+#define DEFAULT_ALIGNMENT __alignof__ (double)
+
+struct pool *pool_create(size_t chunk_hint)
+{
+	struct pool *mem = dbg_malloc(sizeof(*mem));
+
+	if (!mem) {
+		stack;
+		return NULL;
+	}
+
+	mem->blocks = mem->tail = NULL;
+	return mem;
+}
+
+static void _free_blocks(struct block *b)
+{
+	struct block *n;
+
+	while (b) {
+		n = b->next;
+		dbg_free(b);
+		b = n;
+	}
+}
+
+void pool_destroy(struct pool *p)
+{
+	_free_blocks(p->blocks);
+	dbg_free(p);
+}
+
+void *pool_alloc(struct pool *p, size_t s)
+{
+	return pool_alloc_aligned(p, s, DEFAULT_ALIGNMENT);
+}
+
+void *pool_alloc_aligned(struct pool *p, size_t s, unsigned alignment)
+{
+	/* FIXME: I'm currently ignoring the alignment arg. */
+	size_t len = sizeof(struct block) + s;
+	struct block *b = dbg_malloc(len);
+
+	if (!b) {
+		stack;
+		return NULL;
+	}
+
+	b->next = NULL;
+
+	if (p->tail) {
+		p->tail->next = b;
+		p->tail = b;
+	} else
+		p->blocks = p->tail = b;
+
+	return &b->data;
+}
+
+void pool_empty(struct pool *p)
+{
+	_free_blocks(p->blocks);
+	p->blocks = p->tail = NULL;
+}
+
+void pool_free(struct pool *p, void *ptr)
+{
+	struct block *b, *prev = NULL;
+
+	for (b = p->blocks; b; b = b->next) {
+		prev = b;
+		if ((void *) &b->data == ptr)
+			break;
+	}
+
+	/*
+	 * If this fires then you tried to free a
+	 * pointer that either wasn't from this
+	 * pool, or isn't the start of a block.
+	 */
+	assert(b);
+	if (!b) {
+		log_err("pool asked to free a pointer that was not "
+			"allocated from this pool.");
+		assert(0);
+	}
+
+	_free_blocks(b);
+
+	if (prev) {
+		p->tail = prev;
+		prev->next = NULL;
+	} else
+		p->blocks = p->tail = NULL;
+}
+
+void *pool_begin_object(struct pool *p, size_t hint, unsigned align)
+{
+	log_err("pool_begin_object not implemented for pool-debug");
+	assert(0);
+}
+
+void *pool_grow_object(struct pool *p, unsigned char *buffer, size_t n)
+{
+	log_err("pool_grow_object not implemented for pool-debug");
+	assert(0);
+}
+
+void *pool_end_object(struct pool *p)
+{
+	log_err("pool_end_object not implemented for pool-debug");
+	assert(0);
+}
+
+void pool_abandon_object(struct pool *p)
+{
+	log_err("pool_abandon_object not implemented for pool-debug");
+}
+
+char *pool_strdup(struct pool *p, const char *str)
+{
+	char *ret = pool_alloc(p, strlen(str) + 1);
+
+	if (ret)
+		strcpy(ret, str);
+
+	return ret;
+}
