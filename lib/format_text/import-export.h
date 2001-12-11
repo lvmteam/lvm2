d96dea9abf241696a9870eaa214dc99bd65dcfb9
Joe Thornber <thornber@redhat.com>
2001-12-11 12:16:58 +0000

    o  Split import-export.c into two files.

--- lib/format_text/import-export.h
+++ lib/format_text/import-export.h
@@ -7,7 +7,19 @@
 #ifndef _LVM_TEXT_IMPORT_EXPORT_H
 #define _LVM_TEXT_IMPORT_EXPORT_H
 
+#include "config.h"
+
+enum {
+	VG_FLAGS,
+	PV_FLAGS,
+	LV_FLAGS
+};
+
+int print_flags(uint32_t status, int type, char *buffer, size_t size);
+int read_flags(uint32_t *status, int type, struct config_value *cv);
+
+
+int text_vg_export(FILE *fp, struct volume_group *vg);
 struct volume_group *text_vg_import(struct pool *mem, struct config_file *cf);
-struct config_file *text_vg_export(struct pool *mem, struct volume_group *vg);
 
 #endif
