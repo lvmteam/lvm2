83912faa1d452d2309a2cda38338fc995804d463
Alasdair Kergon <agk@redhat.com>
2002-01-08 19:17:08 +0000

    Another attempt to support both readline versions.

--- tools/defaults.h
+++ tools/defaults.h
@@ -10,20 +10,20 @@
 
 #define DEFAULT_SYS_DIR "/etc/lvm"
 
-#define DEFAULT_ARCHIVE_FLAG 1
-#define DEFAULT_BACKUP_FLAG 1
+#define DEFAULT_ARCHIVE_ENABLED 1
+#define DEFAULT_BACKUP_ENABLED 1
 
 #define DEFAULT_ARCHIVE_SUBDIR "archive"
 #define DEFAULT_BACKUP_SUBDIR "backup"
 
-/*
- * FIXME: these are deliberately low for the beta
- * series to encourage testing.
- */
-#define DEFAULT_ARCHIVE_DAYS 7
-#define DEFAULT_ARCHIVE_NUMBER 5
+#define DEFAULT_ARCHIVE_DAYS 30
+#define DEFAULT_ARCHIVE_NUMBER 10
 
 #define DEFAULT_DEV_DIR "/dev"
+
+#ifdef READLINE_SUPPORT
+  #define DEFAULT_MAX_HISTORY 100
+#endif
 
 
 #endif /* _LVM_DEFAULTS_H */
