b06f6b9545dbfe60afd63bfd07ea786be3ee9ed3
Joe Thornber <joe@fib011235813.fsnet.co.uk>
2002-01-14 10:00:56 +0000

    o  LVM1 labeller.

---
+++ lib/format1/lvm1_label.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2002 Sistina Software (UK) Limited.
+ *
+ * This file is released under the LGPL.
+ */
+
+#ifndef _LVM_LVM1_LABEL_H
+#define _LVM_LVM1_LABEL_H
+
+struct labeller *lvm1_labeller_create(void);
+
+#endif
