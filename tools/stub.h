4103182568dc1a481858b7b93c4c85704eb8b3b4
Joe Thornber <thornber@redhat.com>
2002-01-16 11:27:19 +0000

    o  Add pvmove to the stub file.

--- tools/stub.h
+++ tools/stub.h
@@ -9,6 +9,7 @@ int lvmdiskscan(int argc, char **argv) {return 1;}
 int lvmsadc(int argc, char **argv) {return 1;}
 int lvmsar(int argc, char **argv) {return 1;}
 int pvdata(int argc, char **argv) {return 1;}
+int pvmove(int argc, char **argv) {return 1;}
 int vgexport(int argc, char **argv) {return 1;}
 int vgimport(int argc, char **argv) {return 1;}
 int vgmknodes(int argc, char **argv) {return 1;}
