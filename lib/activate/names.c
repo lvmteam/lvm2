131a8a9650ff8a4527f562f4c9e21794c2d1f14d
Joe Thornber <joe@fib011235813.fsnet.co.uk>
2002-01-21 13:11:03 +0000

    o names.[hc]

---
+++ lib/activate/names.c
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2002 Sistina Software (UK) Limited.
+ *
+ * This file is released under the LGPL.
+ */
+
+#include "names.h"
+#include "lvm-string.h"
+#include "log.h"
+#include "limits.h"
+
+#include <libdevmapper.h>
+
+/*
+ * The volume group name and the logical volume name are
+ * seperated by a single ':', any colons in the vg name are
+ * doubled up to form a pair.
+ */
+int build_dm_name(char *buffer, size_t len,
+		  const char *vg_name, const char *lv_name)
+{
+	char *out;
+	const char *in;
+
+	for (out = buffer, in = vg_name; len && *in; len--) {
+		if (*in == ':') {
+			*out++ = ':';
+			if (!--len)
+				break;
+		}
+
+		*out++ = *in++;
+		len--;
+	}
+
+	if (!len)
+		return 0;
+
+	if (lvm_snprintf(out, len, ":%s", lv_name) == -1) {
+		log_err("Couldn't build logical volume name.");
+		return 0;
+	}
+
+	return 1;
+}
+
+int build_dm_path(char *buffer, size_t len,
+		  const char *vg_name, const char *lv_name)
+{
+	char dev_name[PATH_MAX];
+
+	if (!build_dm_name(dev_name, sizeof(dev_name), vg_name, lv_name)) {
+		stack;
+		return 0;
+	}
+
+	if (lvm_snprintf(buffer, len, "%s/%s", dm_dir(), dev_name) == -1) {
+		stack;
+		return 0;
+	}
+
+	return 1;
+}
+
+int build_vg_path(char *buffer, size_t len,
+		  const char *dev_dir, const char *vg_name)
+{
+	if (lvm_snprintf(buffer, len, "%s%s", dev_dir, vg_name) == -1) {
+		stack;
+		return 0;
+	}
+
+	return 1;
+}
+
+int build_lv_link_path(char *buffer, size_t len, const char *dev_dir,
+		       const char *vg_name, const char *lv_name)
+{
+	if (lvm_snprintf(buffer, len, "%s%s/%s",
+			 dev_dir, vg_name, lv_name) == -1) {
+		stack;
+		return 0;
+	}
+
+	return 1;
+}
+
+
---
+++ lib/activate/names.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2002 Sistina Software (UK) Limited.
+ *
+ * This file is released under the LGPL.
+ */
+
+#ifndef _LVM_NAMES_H
+#define _LVM_NAMES_H
+
+#include <stdlib.h>
+
+/*
+ * Functions that build up useful paths to devices, sym-links
+ * etc.  Names are passed in as strings, rather than via the
+ * appropriate metadata structures, so we can use it for renaming
+ * devices.
+ */
+
+
+/*
+ * The name of the device-mapper device for a particular LV.
+ * eg, vg0:music
+ */
+int build_dm_name(char *buffer, size_t len,
+		  const char *vg_name, const char *lv_name);
+
+/*
+ * The path of the device-mapper device for a particular LV.
+ * eg, /dev/device-mapper/vg0:music
+ */
+int build_dm_path(char *buffer, size_t len,
+		  const char *vg_name, const char *lv_name);
+
+/*
+ * Path to the volume group directory.
+ * eg, /dev/vg0
+ */
+int build_vg_path(char *buffer, size_t len,
+		  const char *dev_dir, const char *vg_name);
+
+/*
+ * Path to the symbolic link that lives in the volume group
+ * directory.
+ * eg, /dev/vg0/music
+ */
+int build_lv_link_path(char *buffer, size_t len,
+		       const char *dev_dir,
+		       const char *vg_name, const char *lv_name);
+
+#endif
