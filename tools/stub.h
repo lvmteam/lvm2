f55f9886432c5b5448c436da6db7c4def940f378
Joe Thornber <thornber@redhat.com>
2002-08-08 07:54:57 +0000

    o  Remove e2fsadm to stop people waiting expectantly for something that isn't going    to arrive.

--- tools/stub.h
+++ tools/stub.h
@@ -6,7 +6,6 @@
 
 #define unimplemented \
 	{ log_error("Command not implemented yet."); return ECMD_FAILED;}
-int e2fsadm(struct cmd_context *cmd, int argc, char **argv) unimplemented
 int lvmsadc(struct cmd_context *cmd, int argc, char **argv) unimplemented
 int lvmsar(struct cmd_context *cmd, int argc, char **argv) unimplemented
 int pvdata(struct cmd_context *cmd, int argc, char **argv) unimplemented
