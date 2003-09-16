8a6be4cb2d5511edba0d26e866537bbe923a0f91
Alasdair Kergon <agk@redhat.com>
2003-09-16 16:23:21 +0000

    Remove incorrect comments.

--- lib/metadata/lv_alloc.h
+++ lib/metadata/lv_alloc.h
@@ -2,9 +2,6 @@
  * Copyright (C) 2003 Sistina Software (UK) Limited.
  *
  * This file is released under the LGPL.
- *
- * This is the in core representation of a volume group and its
- * associated physical and logical volumes.
  */
 
 #ifndef _LVM_LV_ALLOC_H
