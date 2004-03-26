7fa2d1a0926c574f47ac1f8230af9cb0e6ad036c
Alasdair Kergon <agk@redhat.com>
2004-03-26 12:00:24 +0000

    Move main() into separate file.

---
+++ tools/lvm2cmdline.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2003 Sistina Software (UK) Limited.
+ *
+ * This file is released under the GPL.
+ */
+
+#ifndef _LVM_CMDLINE_H
+#define _LVM_CMDLINE_H
+
+int lvm2_main(int argc, char **argv);
+
+#endif
