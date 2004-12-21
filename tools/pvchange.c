8c9a1d6a3d9ab42b220b34b90e49d4f6f5c51d80
Alasdair Kergon <agk@redhat.com>
2004-12-21 16:12:02 +0000

    Fix pvchange -x segfault with lvm2-format orphan.

--- tools/pvchange.c
+++ tools/pvchange.c
@@ -32,6 +32,8 @@ static int _pvchange_single(struct cmd_context *cmd, struct physical_volume *pv,
 	int allocatable = 0;
 	int tagarg = 0;
 
+	list_init(&mdas);
+
 	if (arg_count(cmd, addtag_ARG))
 		tagarg = addtag_ARG;
 	else if (arg_count(cmd, deltag_ARG))
