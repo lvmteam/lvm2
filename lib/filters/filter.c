c44c015a2d3bf0d2f5605c062c03375b4f73cd27
Alasdair Kergon <agk@redhat.com>
2005-01-19 18:56:01 +0000

    Recognise gnbd.

--- lib/filters/filter.c
+++ lib/filters/filter.c
@@ -69,6 +69,7 @@ static const device_info_t device_info[] = {
 	{"power2", 16},		/* EMC Powerpath */
 	{"i2o_block", 16},	/* i2o Block Disk */
 	{"iseries/vd", 8},	/* iSeries disks */
+	{"gnbd", 1},		/* Network block device */
 	{NULL, 0}
 };
 
