c8c07bbedcbe0d208ed203d8e24f34a980e154e0
Alasdair Kergon <agk@redhat.com>
2005-06-13 13:11:48 +0000

    Use matchpathcon mode parameter

--- libdm/ioctl/libdm-iface.c
+++ libdm/ioctl/libdm-iface.c
@@ -219,7 +219,7 @@ static int _create_control(const char *control, uint32_t major, uint32_t minor)
 	}
 
 #ifdef HAVE_SELINUX
-        if (!set_selinux_context(control)) {
+        if (!set_selinux_context(control, S_IFCHR)) {
                 stack;
                 return 0;
         }
