05f5abdc0686e4b9f33843124bfb5fb24859368b
Alasdair Kergon <agk@redhat.com>
2006-11-16 16:44:48 +0000

    Enhance lvm_dump.sh for sysreport integration and add man page.

--- scripts/lvm_dump.sh
+++ scripts/lvm_dump.sh
@@ -10,7 +10,7 @@ function usage {
 	echo "    -a advanced collection - warning: if lvm is already hung,"
 	echo "       then this script may hang as well if -a is used"
 	echo "    -m gather LVM metadata from the PVs"
-	echo "    -d dump directory to place data in (default=/tmp/lvm_dump.\$\$)"
+	echo "    -d <directory> dump into a directory instead of tarball"
 	echo "    -c if running clvmd, gather cluster data as well"
 	echo ""
 	
@@ -22,9 +22,10 @@ clustered=0
 metadata=0
 while getopts :acd:hm opt; do
 	case $opt in 
+		s)      sysreport=1 ;;
 		a)	advanced=1 ;;
 		c)	clustered=1 ;;
-		d)	lvm_dir=$OPTARG ;;
+		d)	userdir=$OPTARG ;;
 		h)	usage ;;
 		m)	metadata=1 ;;
 		:)	echo "$0: $OPTARG requires a value:"; usage ;;
@@ -33,86 +34,119 @@ while getopts :acd:hm opt; do
 	esac
 done
 
-dir=`mktemp -d -p /tmp lvm_dump.XXXXXX` || exit 2
-lvm_dir="$dir/lvm_dump"
+DATE=`/bin/date -u +%G%m%d%k%M%S | /usr/bin/tr -d ' '`
+if test -n "$userdir"; then
+	dir="$userdir"
+else
+	dirbase="lvmdump-$HOSTNAME-$DATE"
+	dir="$HOME/$dirbase"
+fi
+
+if test -e $dir; then
+	echo $dir already exists, aborting >&2
+	exit 2
+fi
+
+if ! mkdir -p $dir; then
+	echo Could not create $dir >&2
+	exit 3
+fi
+
+log="$dir/lvmdump.log"
+
+myecho() {
+	echo "$@"
+	echo "$@" >> $log
+}
+
+log() {
+	echo "$@" >> $log
+	eval "$@"
+}
 
 echo " "
-echo "Creating dump directory: $lvm_dir"
+myecho "Creating dump directory: $dir"
 echo " "
-
-mkdir -p $lvm_dir || exit 3
 
 if (( $advanced )); then
-	echo "Gathering LVM volume info..."
+	myecho "Gathering LVM volume info..."
 
-	echo "  vgscan..."
-	vgscan -vvvv > $lvm_dir/vgscan 2>&1
+	myecho "  vgscan..."
+	log "vgscan -vvvv > $dir/vgscan 2>&1"
 
-	echo "  pvscan..."
-	pvscan -v >> $lvm_dir/pvscan 2>/dev/null
+	myecho "  pvscan..."
+	log "pvscan -v >> $dir/pvscan 2>> $log"
 
-	echo "  lvs..."
-	lvs -a -o +devices >> $lvm_dir/lvs 2>/dev/null
+	myecho "  lvs..."
+	log "lvs -a -o +devices >> $dir/lvs 2>> $log"
 
-	echo "  pvs..."
-	pvs -a -v > $lvm_dir/pvs 2>/dev/null
+	myecho "  pvs..."
+	log "pvs -a -v > $dir/pvs 2>> $log"
 
 	echo "  vgs..."
-	vgs -v > $lvm_dir/vgs 2>/dev/null
+	log "vgs -v > $dir/vgs 2>> $log"
 fi
 
 if (( $clustered )); then
-	echo "Gathering cluster info..."
-	echo "STATUS: " > $lvm_dir/cluster_info
-	echo "----------------------------------" >> $lvm_dir/cluster_info
-	cman_tool status >> $lvm_dir/cluster_info
-	echo " " >> $lvm_dir/lvm_info
+	myecho "Gathering cluster info..."
+	echo "STATUS: " > $dir/cluster_info
+	echo "----------------------------------" >> $dir/cluster_info
+	log "cman_tool status >> $dir/cluster_info 2>> $log"
+	echo " " >> $dir/lvm_info
 
-	echo "SERVICES: " >> $lvm_dir/cluster_info
-	echo "----------------------------------" >> $lvm_dir/cluster_info
-	cman_tool services >> $lvm_dir/cluster_info
-	echo " " >> $lvm_dir/lvm_info
+	echo "SERVICES: " >> $dir/cluster_info
+	echo "----------------------------------" >> $dir/cluster_info
+	log "cman_tool services >> $dir/cluster_info 2>> $log"
+	echo " " >> $dir/lvm_info
 fi
 
-echo "Gathering LVM & device-mapper version info..."
-echo "LVM VERSION:" > $lvm_dir/versions
-lvs --version >> $lvm_dir/versions
-echo "DEVICE MAPPER VERSION:" >> $lvm_dir/versions
-dmsetup --version >> $lvm_dir/versions
+myecho "Gathering LVM & device-mapper version info..."
+echo "LVM VERSION:" > $dir/versions
+lvs --version >> $dir/versions 2>> $log
+echo "DEVICE MAPPER VERSION:" >> $dir/versions
+dmsetup --version >> $dir/versions 2>> $log
 
-echo "Gathering dmsetup info..."
-dmsetup info -c > $lvm_dir/dmsetup_info
-dmsetup table > $lvm_dir/dmsetup_table
-dmsetup status > $lvm_dir/dmsetup_status
+myecho "Gathering dmsetup info..."
+log "dmsetup info -c > $dir/dmsetup_info 2>> $log"
+log "dmsetup table > $dir/dmsetup_table 2>> $log"
+log "dmsetup status > $dir/dmsetup_status 2>> $log"
 
-echo "Gathering process info..."
-ps alx > $lvm_dir/ps_info
+myecho "Gathering process info..."
+log "ps alx > $dir/ps_info 2>> $log"
 
-echo "Gathering console messages..."
-tail -n 75 /var/log/messages > $lvm_dir/messages
+myecho "Gathering console messages..."
+log "tail -n 75 /var/log/messages > $dir/messages 2>> $log"
 
-echo "Gathering /etc/lvm info..."
-cp -a /etc/lvm $lvm_dir/lvm
+myecho "Gathering /etc/lvm info..."
+log "cp -a /etc/lvm $dir/lvm 2>> $log"
 
-echo "Gathering /dev listing..."
-ls -la /dev > $lvm_dir/dev_listing
+myecho "Gathering /dev listing..."
+log "ls -la /dev > $dir/dev_listing 2>> $log"
 
 if (( $metadata )); then
-	echo "Gathering LVM metadata from Physical Volumes..."
+	myecho "Gathering LVM metadata from Physical Volumes..."
 
-	mkdir -p $lvm_dir/metadata
+	log "mkdir -p $dir/metadata"
 
-	for pv in `pvs --noheadings -o name`
+	pvs="$(pvs --separator , --noheadings --units s --nosuffix -o name,pe_start 2>> $log | \
+		sed -e 's/^ *//')"
+	for line in "$pvs"
 	do
-		echo "  $pv"
-		name=`basename $pv`
-		dd if=$pv of=$lvm_dir/metadata/$name bs=512 count=`pvs --noheadings --nosuffix --units s -o pe_start $pv | tr -d \ `
-	done 2>/dev/null
+		pv="$(echo $line | cut -d, -f1)"
+		pe_start="$(echo $line | cut -d, -f2)"
+		name="$(basename $pv)"
+		myecho "  $pv"
+		log "dd if=$pv of=$dir/metadata/$name bs=512 count=$pe_start 2>> $log"
+	done
 fi
 
-lvm_dump=$lvm_dir.tgz
-echo "Creating tarball $lvm_dump..."
-tar czf $lvm_dump $lvm_dir 2>/dev/null
+if test -z "$userdir"; then
+	lvm_dump="$dirbase.tgz"
+	myecho "Creating report tarball in $HOME/$lvm_dump..."
+	cd $HOME
+	tar czf $lvm_dump $dirbase 2>/dev/null
+	rm -rf $dir
+fi
 
 exit 0
 
