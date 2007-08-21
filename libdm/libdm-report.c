1ad58e112130ac3f456723861be53b4307a3452d
Alasdair Kergon <agk@redhat.com>
2007-08-21 16:26:07 +0000

    Fix inconsistent licence notices: executables are GPLv2; libraries LGPLv2.1.

--- libdm/libdm-report.c
+++ libdm/libdm-report.c
@@ -1,15 +1,14 @@
 /*
  * Copyright (C) 2002-2004 Sistina Software, Inc. All rights reserved.
- * Copyright (C) 2004 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2004-2007 Red Hat, Inc. All rights reserved.
  *
- * This file is part of device-mapper userspace tools.
- * The code is based on LVM2 report function.
+ * This file is part of the device-mapper userspace tools.
  *
  * This copyrighted material is made available to anyone wishing to use,
  * modify, copy, or redistribute it subject to the terms and conditions
- * of the GNU General Public License v.2.
+ * of the GNU Lesser General Public License v.2.1.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
