91953ca3aa9a89f6353417d8e13e8c0b28cc4605
Alasdair Kergon <agk@redhat.com>
2007-08-21 19:56:18 +0000

    Fix (C) !

--- tools/args.h
+++ tools/args.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C, 0) 2001-2004 Sistina Software, Inc. All rights reserved.  
- * Copyright (C, 0) 2004-2007 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2001-2004 Sistina Software, Inc. All rights reserved.  
+ * Copyright (C) 2004-2007 Red Hat, Inc. All rights reserved.
  *
  * This file is part of LVM2.
  *
