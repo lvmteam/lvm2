b4ad9a5d0840b41a37500e738a3bc481d0c62a90
Alasdair Kergon <agk@redhat.com>
2008-04-09 12:56:34 +0000

    Use clustered mirror log with pvmove in clustered VGs, if available.

--- tools/polldaemon.h
+++ tools/polldaemon.h
@@ -29,7 +29,7 @@ struct poll_functions {
 	int (*update_metadata) (struct cmd_context *cmd,
 				struct volume_group *vg,
 				struct logical_volume *lv_mirr,
-				struct list *lvs_changed, int first_time);
+				struct list *lvs_changed, unsigned flags);
 	int (*finish_copy) (struct cmd_context *cmd,
 			    struct volume_group *vg,
 			    struct logical_volume *lv_mirr,
