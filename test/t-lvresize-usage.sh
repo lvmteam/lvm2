e612871ea72d9f2c81e7084994454a0fe328759e
Alasdair Kergon <agk@redhat.com>
2008-04-10 19:59:43 +0000

    more pre-release cleanup

--- test/t-lvresize-usage.sh
+++ test/t-lvresize-usage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2007 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2007-2008 Red Hat, Inc. All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
