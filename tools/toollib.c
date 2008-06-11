a78d7231a90c7e2db4391f989d4d9f3e67b49b8b
Jim Meyering <jim@meyering.net>
2008-06-11 15:02:52 +0000

    break (rather than return) so we have only one point of return
    * tools/toollib.c (process_each_segment_in_lv): Upon sigint_caught,
    break rather than returning ret_max.  No semantic change.

--- tools/toollib.c
+++ tools/toollib.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2001-2004 Sistina Software, Inc. All rights reserved.
- * Copyright (C) 2004-2007 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2004-2008 Red Hat, Inc. All rights reserved.
  *
  * This file is part of LVM2.
  *
@@ -404,7 +404,7 @@ int process_each_lv(struct cmd_context *cmd, int argc, char **argv,
 		if (ret > ret_max)
 			ret_max = ret;
 		if (sigint_caught())
-			return ret_max;
+			break;
 	}
 
 	return ret_max;
