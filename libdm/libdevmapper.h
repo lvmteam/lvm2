f1839ac87c92a0d87141e3c7474d7c60930dc300
Alasdair Kergon <agk@redhat.com>
2008-06-24 20:16:47 +0000

    dmsetup --unquoted

--- libdm/libdevmapper.h
+++ libdm/libdevmapper.h
@@ -740,6 +740,7 @@ struct dm_report_field_type {
 #define DM_REPORT_OUTPUT_BUFFERED		0x00000002
 #define DM_REPORT_OUTPUT_HEADINGS		0x00000004
 #define DM_REPORT_OUTPUT_FIELD_NAME_PREFIX	0x00000008
+#define DM_REPORT_OUTPUT_FIELD_UNQUOTED		0x00000010
 
 struct dm_report *dm_report_init(uint32_t *report_types,
 				 const struct dm_report_object_type *types,
