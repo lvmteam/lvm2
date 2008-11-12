1c38ce82454e6df21285aaec1dce35c77c001145
Zdeněk Kabeláč <zkabelac@redhat.com>
2008-11-12 09:53:33 +0000

    cleaner const char* usage for last_path_component()

--- lib/misc/last-path-component.h
+++ lib/misc/last-path-component.h
@@ -19,10 +19,9 @@
 
 #include <string.h>
 
-static inline char *last_path_component(char const *name)
+static inline const char *last_path_component(char const *name)
 {
 	char const *slash = strrchr(name, '/');
-	char const *res = slash ? slash + 1 : name;
 
-	return (char *)res;
+	return (slash) ? slash + 1 : name;
 }
