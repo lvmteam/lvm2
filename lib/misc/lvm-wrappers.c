b721056e04ddd38564898b323caf8c559bf88cb7
Dave Wysochanski <dwysocha@redhat.com>
2008-12-07 04:23:37 +0000

    Add generic function to read /dev/urandom, used in uuid calculation.

--- lib/misc/lvm-wrappers.c
+++ lib/misc/lvm-wrappers.c
@@ -15,8 +15,34 @@
 #include "lib.h"
 
 #include <unistd.h>
+#include <fcntl.h>
 
 int lvm_getpagesize(void)
 {
 	return getpagesize();
 }
+
+int read_urandom(void *buf, size_t len)
+{
+	int fd;
+
+	/* FIXME: we should stat here, and handle other cases */
+	/* FIXME: use common _io() routine's open/read/close */
+	if ((fd = open("/dev/urandom", O_RDONLY)) < 0) {
+		log_sys_error("open", "read_urandom: /dev/urandom");
+		return 0;
+	}
+
+	if (read(fd, buf, len) != (ssize_t) len) {
+		log_sys_error("read", "read_urandom: /dev/urandom");
+		if (close(fd))
+			stack;
+		return 0;
+	}
+
+	if (close(fd))
+		stack;
+
+	return 1;
+}
+
