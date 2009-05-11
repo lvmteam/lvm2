9d81c953c339cff692936f1c480d843546a6eaca
Dave Wysochanski <dwysocha@redhat.com>
2009-05-11 16:17:12 +0000

    Update t-read-ahead.sh to validate lv_read_ahead and lv_kernel_read_ahead.
    - check default values
    - check active/inactive values

--- test/t-read-ahead.sh
+++ test/t-read-ahead.sh
@@ -43,3 +43,13 @@ lvchange -r 400 "$vg/$lv"
 check_lvs_ lv_read_ahead 400                                   
 lvremove -ff "$vg"
 
+# Check default, active/inactive values for read_ahead / kernel_read_ahead
+lvcreate -n $lv -l 50%FREE $vg
+lvchange -an $vg/$lv
+check_lv_field_ $vg/$lv lv_read_ahead auto
+check_lv_field_ $vg/$lv lv_kernel_read_ahead -1
+lvchange -r 512 $vg/$lv
+lvchange -ay $vg/$lv
+check_lv_field_ $vg/$lv lv_read_ahead 256.00K
+check_lv_field_ $vg/$lv lv_kernel_read_ahead 256.00K
+lvremove -ff $vg
