19089ba3311945a75e55df21e35a02b19177350f
Petr Rockai <prockai@redhat.com>
2009-07-15 05:47:55 +0000

    Refuse to open VG with MISSING_PVs for update unless handles_missing_pvs is set.

--- test/t-partial-activate.sh
+++ test/t-partial-activate.sh
@@ -9,3 +9,7 @@ disable_dev $dev1
 not vgreduce --removemissing $vg
 not lvchange -v -a y $vg/mirror
 lvchange -v --partial -a y $vg/mirror
+
+# also check that vgchange works
+vgchange -a n --partial $vg
+vgchange -a y --partial $vg
