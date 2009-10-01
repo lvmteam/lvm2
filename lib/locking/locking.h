3435f247701015d1a7f7d896bde44239186c79e8
Christine Caulfield <ccaulfie@redhat.com>
2009-10-01 14:15:34 +0000

    Add LCK_CONVERT flag I missed on the last checkin.

--- lib/locking/locking.h
+++ lib/locking/locking.h
@@ -90,6 +90,7 @@ int check_lvm1_vg_inactive(struct cmd_context *cmd, const char *vgname);
 #define LCK_PARTIAL_MODE        0x00000001U	/* Partial activation? */
 #define LCK_MIRROR_NOSYNC_MODE	0x00000002U	/* Mirrors don't require sync */
 #define LCK_DMEVENTD_MONITOR_MODE	0x00000004U	/* Register with dmeventd */
+#define LCK_CONVERT		0x00000008U	/* Convert existing lock */
 
 /*
  * Special cases of VG locks.
