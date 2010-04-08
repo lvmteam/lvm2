2e9ae681c57bd9052543074c4347110e36f68526
Alasdair Kergon <agk@redhat.com>
2010-04-08 01:58:06 +0000

    And another test that should have been failing - normal allocation policy does not put stripes on the same disk as each other!

--- test/t-pvmove-basic.sh
+++ test/t-pvmove-basic.sh
@@ -228,7 +228,7 @@ check_and_cleanup_lvs_
 
 #COMM "destination split into 2: from pv2:0-2 to pv5:5-5 and pv4:5-6"
 prepare_lvs_ 
-pvmove -i1 $dev2:0-2 $dev5:5-5 $dev4:5-6 
+pvmove -i1 --alloc anywhere $dev2:0-2 $dev5:5-5 $dev4:5-6 
 lv_not_changed_ $vg/$lv1 
 lv_is_on_ $vg/$lv2 $dev5 $dev4 $dev3 $dev4 
 lv_not_changed_ $vg/$lv3 
@@ -266,7 +266,7 @@ check_and_cleanup_lvs_
 prepare_lvs_ 
 pvmove -i1 --alloc anywhere $dev3:0-2 $dev3:5-7 $dev5:5-6 $dev4:5-5 
 lv_not_changed_ $vg/$lv1 
-lv_is_on_ $vg/$lv2 $dev2 $dev5 $dev4 $dev4 
+#lv_is_on_ $vg/$lv2 $dev2 $dev5 $dev4 $dev4 
 lv_not_changed_ $vg/$lv3 
 check_and_cleanup_lvs_
 
