312a15d24eaaaace927d919f05b568d3b4ba1976
Zdenek Kabelac <zkabelac@redhat.com>
2010-05-21 12:36:30 +0000

    Replicator: base lvm2 support
    Adding configure.in support for Replicators.
    Adding basic lib lvm support for Replicators.
    Adding flags REPLICATOR and REPLICATOR_LOG.
    Adding segments SEG_REPLICATOR and SEG_REPLICATOR_DEV.
    Adding basic methods for handling replicator metadata.

--- lib/metadata/lv_manip.c
+++ lib/metadata/lv_manip.c
@@ -2008,6 +2008,7 @@ struct logical_volume *alloc_lv(struct dm_pool *mem)
 	dm_list_init(&lv->segments);
 	dm_list_init(&lv->tags);
 	dm_list_init(&lv->segs_using_this_lv);
+	dm_list_init(&lv->rsites);
 
 	return lv;
 }
