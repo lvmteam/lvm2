90b96af68fbe714ce5bb7b0eb0f383faecb1e6b5
Dave Wysochanski <dwysocha@redhat.com>
2010-07-07 19:02:50 +0000

    Add prompt if using --metadataignore argument with vgmetadatacopies.
    When using vgmetadatacopies value other than "umanaged" (0), prompt
    the user if the usage of --metadataignore would change the value of
    vgmetadatacopies.  The main 2 cases are:
    1) pvchange --metadataignore
    2) vgextend --metadataignore
    
    We leave the prompt check in the tools, and do not change anything
    if the user says 'n'.
    
    Examples:
    vgextend --metadataignore y vgtest /dev/loop0
    Setting metadataignore will override preferred number of copies of VG vgtest metadata.
    Are you sure? [y/n]: y
      No physical volume label read from /dev/loop0
      Physical volume "/dev/loop0" successfully created
      Volume group "vgtest" successfully extended
    
    pvchange --metadataignore y /dev/loop3
    Setting metadataignore on /dev/loop3 will override preferred number of copies of VG vgtest metadata.
    Are you sure? [y/n]: y
      WARNING: Changing preferred number of copies of VG vgtest metadata from 3 to 2
      Physical volume "/dev/loop3" changed
      1 physical volume changed / 0 physical volumes not changed
    
    Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>

--- tools/pvchange.c
+++ tools/pvchange.c
@@ -122,6 +122,14 @@ static int _pvchange_single(struct cmd_context *cmd, struct volume_group *vg,
 			}
 		}
 	} else if (arg_count(cmd, metadataignore_ARG)) {
+		if ((vg_mda_copies(vg) != VGMETADATACOPIES_UNMANAGED) &&
+		    yes_no_prompt("Setting metadataignore on %s will override "
+				  "preferred number of copies of VG %s "
+				  "metadata.\nAre you sure? [y/n]: ",
+				  pv_name, pv_vg_name(pv)) == 'n') {
+			log_error("Physical volume %s not changed", pv_name);
+			goto out;
+		}
 		if (!pv_change_metadataignore(pv, mda_ignore))
 			goto out;
 	} else {
