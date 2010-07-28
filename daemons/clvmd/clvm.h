fb3b7f2274dae6d8e5f9ad5c001bca233fd4969a
Alasdair Kergon <agk@redhat.com>
2010-07-28 13:55:42 +0000

    Change clvmd to communicate with lvm via a socket in /var/run/lvm.   (mbroz)
    https://bugzilla.redhat.com/show_bug.cgi?id=614248 [CVE-2010-2526]

--- daemons/clvmd/clvm.h
+++ daemons/clvmd/clvm.h
@@ -22,6 +22,8 @@
 #ifndef _CLVM_H
 #define _CLVM_H
 
+#include "configure.h"
+
 struct clvm_header {
 	uint8_t  cmd;	        /* See below */
 	uint8_t  flags;	        /* See below */
@@ -45,9 +47,8 @@ struct clvm_header {
 #define CLVMD_FLAG_SYSTEMLV     2	/* Data in system LV under my node name */
 #define CLVMD_FLAG_NODEERRS     4       /* Reply has errors in node-specific portion */
 
-/* Name of the local socket to communicate between libclvm and clvmd */
-//static const char CLVMD_SOCKNAME[]="/var/run/clvmd";
-static const char CLVMD_SOCKNAME[] = "\0clvmd";
+/* Name of the local socket to communicate between lvm and clvmd */
+static const char CLVMD_SOCKNAME[]= DEFAULT_RUN_DIR "/clvmd.sock";
 
 /* Internal commands & replies */
 #define CLVMD_CMD_REPLY    1
