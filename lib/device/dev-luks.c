8c8a7cbba412ff4e12fc1d93e3d08173d1e07653
Milan Broz <mbroz@redhat.com>
2010-08-19 23:08:18 +0000

    Detect LUKS signature in pvcreate
    One shiny day we should use libblkid here. But now using LUKS is
    very common together with LVM and pvcreate destroys LUKS completely.
    
    So for user's convenience, try to detect LUKS signature and allow abort.

---
+++ lib/device/dev-luks.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2010 Red Hat, Inc. All rights reserved.
+ *
+ * This file is part of LVM2.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License v.2.1.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "lib.h"
+#include "metadata.h"
+
+#define LUKS_SIGNATURE "LUKS\xba\xbe"
+#define LUKS_SIGNATURE_SIZE 6
+
+int dev_is_luks(struct device *dev, uint64_t *signature)
+{
+	char buf[LUKS_SIGNATURE_SIZE];
+	int ret = -1;
+
+	if (!dev_open(dev)) {
+		stack;
+		return -1;
+	}
+
+	*signature = 0;
+
+	if (!dev_read(dev, 0, LUKS_SIGNATURE_SIZE, buf))
+		goto_out;
+
+	ret = memcmp(buf, LUKS_SIGNATURE, LUKS_SIGNATURE_SIZE) ? 0 : 1;
+
+out:
+	if (!dev_close(dev))
+		stack;
+
+	return ret;
+}
