af579eccc34be2943ef2be7de712a8a7f09b3df4
Dave Wysochanski <dwysocha@redhat.com>
2010-10-12 16:12:02 +0000

    Refactor and add code for (lv) 'move_pv' get function.
    Signed-off-by: Dave Wysochanski <dwysocha@redhat.com>
    Reviewed-By: Petr Rockai <prockai@redhat.com>

--- lib/metadata/lv.h
+++ lib/metadata/lv.h
@@ -54,5 +54,6 @@ char *lv_uuid_dup(const struct logical_volume *lv);
 char *lv_tags_dup(const struct logical_volume *lv);
 char *lv_path_dup(struct dm_pool *mem, const struct logical_volume *lv);
 uint64_t lv_origin_size(const struct logical_volume *lv);
+char *lv_move_pv_dup(struct dm_pool *mem, const struct logical_volume *lv);
 
 #endif
