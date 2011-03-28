012b164bae8ce840976f192d149d93703b110ac1
Zdenek Kabelac <zkabelac@redhat.com>
2011-03-28 11:35:20 +0000

    Reduce amount of vgremove and vgchange calls
    A bit noticable time defference when whole test-suite is run through valgrind.

--- test/t-vgmerge-usage.sh
+++ test/t-vgmerge-usage.sh
@@ -31,24 +31,21 @@ vgcreate  $vg1 $dev1 $dev2
 vgcreate  $vg2 $dev3 $dev4 
 not vgmerge $vg1 $vg1 2>err
 grep "^  Duplicate volume group name \"$vg1\"\$" err 
-vgremove $vg2 
-vgremove $vg1
+vgremove $vg1 $vg2
 
 # 'vgmerge rejects vgs with incompatible extent_size'
 vgcreate  --physicalextentsize 4M $vg1 $dev1 $dev2 
 vgcreate  --physicalextentsize 8M $vg2 $dev3 $dev4 
 not vgmerge $vg1 $vg2 2>err
 grep "^  Extent sizes differ" err 
-vgremove $vg2 
-vgremove $vg1
+vgremove $vg1 $vg2
 
 # 'vgmerge rejects vgmerge because max_pv is exceeded'
 vgcreate  --maxphysicalvolumes 2 $vg1 $dev1 $dev2 
 vgcreate  --maxphysicalvolumes 2 $vg2 $dev3 $dev4 
 not vgmerge $vg1 $vg2 2>err
 grep "^  Maximum number of physical volumes (2) exceeded" err 
-vgremove $vg2 
-vgremove $vg1
+vgremove $vg1 $vg2
 
 # 'vgmerge rejects vg with active lv'
 vgcreate $vg1 $dev1 $dev2 
@@ -56,8 +53,7 @@ vgcreate $vg2 $dev3 $dev4
 lvcreate -l 4 -n lv1 $vg2 
 not vgmerge $vg1 $vg2 2>err
 grep "^  Logical volumes in \"$vg2\" must be inactive\$" err 
-vgremove -f $vg2 
-vgremove -f $vg1
+vgremove -f $vg1 $vg2
 
 # 'vgmerge rejects vgmerge because max_lv is exceeded' 
 vgcreate --maxlogicalvolumes 2 $vg1 $dev1 $dev2 
@@ -65,9 +61,7 @@ vgcreate --maxlogicalvolumes 2 $vg2 $dev3 $dev4
 lvcreate -l 4 -n lv1 $vg1 
 lvcreate -l 4 -n lv2 $vg1 
 lvcreate -l 4 -n lv3 $vg2 
-vgchange -an $vg1 
-vgchange -an $vg2 
+vgchange -an $vg1 $vg2
 not vgmerge $vg1 $vg2 2>err
 grep "^  Maximum number of logical volumes (2) exceeded" err 
-vgremove -f $vg2 
-vgremove -f $vg1
+vgremove -f $vg1 $vg2
