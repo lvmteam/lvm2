e01642db66f1dcd54acb07c9ad2ec888a8bea285
Peter Rajnoha <prajnoha@redhat.com>
2011-06-03 09:05:30 +0000

    Fix a few compile warnings.
    clvmd-command.c:84:2: warning: implicit declaration of function ‘init_test’
    lvm-functions.c:141:3: warning: too many arguments for format

--- daemons/clvmd/lvm-functions.c
+++ daemons/clvmd/lvm-functions.c
@@ -132,7 +132,7 @@ static const char *decode_flags(unsigned char flags)
 	static char buf[128];
 	int len;
 
-	len = sprintf(buf, "0x%x ( %s%s%s%s%s)", flags,
+	len = sprintf(buf, "0x%x ( %s%s%s%s%s%s)", flags,
 		flags & LCK_PARTIAL_MODE	  ? "PARTIAL_MODE|" : "",
 		flags & LCK_MIRROR_NOSYNC_MODE	  ? "MIRROR_NOSYNC|" : "",
 		flags & LCK_DMEVENTD_MONITOR_MODE ? "DMEVENTD_MONITOR|" : "",
