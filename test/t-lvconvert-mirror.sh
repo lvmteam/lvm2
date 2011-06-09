a5437124318dfff3d697e94917c9a264d7122a03
Milan Broz <mbroz@redhat.com>
2011-06-09 19:38:56 +0000

    Fix mirrored stripe reduction.
    Patch adds check for stripe not only in direct
    LV segment but also in mirror image segment.
    
    This prevents bugs like:
    
    # lvcreate -i2 -l10 -n lv vg_test
    # lvconvert -m1 -i1 vg_test/lv
    
    # lvreduce -f -l1 vg_test/lv
      WARNING: Reducing active logical volume to 4.00 MiB
      THIS MAY DESTROY YOUR DATA (filesystem etc.)
      Reducing logical volume lv to 4.00 MiB
      Segment extent reduction 9 not divisible by #stripes 2
      Logical volume lv successfully resized
    
    # lvremove -f vg_test
      Segment extent reduction 1 not divisible by #stripes 2
      LV segment lv:0-4294967295 is incorrectly listed as being used by LV lv_mimage_0
      Internal error: LV segments corrupted in lv_mimage_0.

--- test/t-lvconvert-mirror.sh
+++ test/t-lvconvert-mirror.sh
@@ -240,3 +240,11 @@ lvconvert -m+1 --mirrorlog disk -i1 $vg/$lv1 $dev4 $dev3:0
 check mirror $vg $lv1 $dev3
 check mirror_no_temporaries $vg $lv1
 check mirror_legs $vg $lv1 3
+
+# simple mirrored stripe
+aux prepare_vg 5
+lvcreate -i2 -l10 -n $lv1 $vg
+lvconvert -m1 -i1 $vg/$lv1
+lvreduce -f -l1 $vg/$lv1
+lvextend -f -l10 $vg/$lv1
+lvremove -ff $vg/$lv1
