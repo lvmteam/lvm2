f20e649ad1dd6df42ddf0746d86caf835ae0a7ae
Petr Rockai <prokai@redhat.com>
2011-07-18 14:48:30 +0000

    Start filling in the core LVMetaD functionality and the corresponding testclient bits.

--- daemons/lvmetad/testclient.c
+++ daemons/lvmetad/testclient.c
@@ -1,14 +1,47 @@
 #include "lvmetad-client.h"
 
+const char *uuid1 = "abcd-efgh";
+const char *uuid2 = "bbcd-efgh";
+const char *vgid = "yada-yada";
+
+const char *metadata2 = "{\n"
+	"id = \"yada-yada\"\n"
+	"seqno = 15\n"
+	"status = [\"READ\", \"WRITE\"]\n"
+	"flags = []\n"
+	"extent_size = 8192\n"
+	"physical_volumes {\n"
+	"    pv0 {\n"
+	"        id = \"abcd-efgh\"\n"
+	"    }\n"
+	"    pv1 {\n"
+	"        id = \"bbcd-efgh\"\n"
+	"    }\n"
+	"}\n"
+	"}\n";
+
+void pv_add(daemon_handle h, const char *uuid, const char *metadata)
+{
+	daemon_reply reply = daemon_send_simple(h, "pv_add", "uuid = %s", uuid,
+						             "metadata = %b", metadata,
+						             NULL);
+	const char *repl = daemon_reply_str(reply, "response", NULL);
+	fprintf(stderr, "[C] REPLY: %s\n", repl);
+	if (!strcmp(repl, "failed"))
+		fprintf(stderr, "[C] REASON: %s\n", daemon_reply_str(reply, "reason", "unknown"));
+	daemon_reply_destroy(reply);
+}
+
 int main() {
 	daemon_handle h = lvmetad_open();
-	int i;
-	for (i = 0; i < 5; ++i ) {
-		daemon_reply reply = daemon_send_simple(h, "hello world", "param = %d", 3, NULL);
-		fprintf(stderr, "[C] REPLY: %s, param = %d\n", daemon_reply_str(reply, "request", "NONE"),
-			                                       daemon_reply_int(reply, "param", -1));
-		daemon_reply_destroy(reply);
-	}
+
+	pv_add(h, uuid1, NULL);
+	pv_add(h, uuid2, metadata2);
+
+	daemon_reply reply = daemon_send_simple(h, "vg_by_uuid", "uuid = %s", vgid, NULL);
+	fprintf(stderr, "[C] reply buffer: %s\n", reply.buffer);
+	daemon_reply_destroy(reply);
+
 	daemon_close(h);
 	return 0;
 }
