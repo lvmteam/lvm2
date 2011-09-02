0031b789381f4330b4d04f3cf0ea8196bee6e2e1
Alasdair Kergon <agk@redhat.com>
2011-09-02 01:59:07 +0000

    temp notes on dealing with cascade

--- lib/config/config.c
+++ lib/config/config.c
@@ -45,6 +45,7 @@ void destroy_config_tree(struct dm_config_tree *cft)
  */
 struct dm_config_tree *remove_overridden_config_tree(struct cmd_context *cmd)
 {
+// FIXME Replace cmd->cft with clean copy of merged lvm*.conf tree
 	struct dm_config_tree *old_cft = cmd->cft;
 	struct dm_config_tree *cft = dm_config_remove_cascaded_tree(cmd->cft);
 
@@ -56,6 +57,8 @@ struct dm_config_tree *remove_overridden_config_tree(struct cmd_context *cmd)
 	return old_cft;
 }
 
+// FIXME Retain a copy of the string (or tree?) in cmd->cft_cmdline
+// FIXME and merge into cmd->cft
 int override_config_tree_from_string(struct cmd_context *cmd,
 				     const char *config_settings)
 {
