867e4cfffbd5037a4d897b1c37e25a0779095cfa
Zdenek Kabelac <zkabelac@redhat.com>
2011-09-07 08:31:16 +0000

    Replace char class :space: with explicit chars
    Some major distributions are still using 'mawk' and they are not using
    the latest version - we end here with hidden dependency on the latest
    version of mawk (1.3.4) while i.e. Debian Lenny seems to stay with 1.3.3.
    So we end with completely broken  vgimportclone script on such system.
    
    We would need to check for proper support of :space: and abort build if
    it doesn't work or simplier replace [:space:] with [ \t] which seems
    sufficient to make it work (as can be seen in this patch)
    
    A better fix would be to use command line parameter override - leaving
    as FIXME comment.
    
    This patch makes t-vgimportclone.sh test passing on Lenny.

--- test/t-vgimportclone.sh
+++ test/t-vgimportclone.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2010 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2010-2011 Red Hat, Inc. All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
@@ -12,25 +12,19 @@
 
 aux prepare_devs 2
 
-pvcreate $dev1 $dev2
-vgcreate $vg1 $dev1
+vgcreate -c n --metadatasize 128k $vg1 $dev1
 lvcreate -l100%FREE -n $lv1 $vg1
 
 # Clone the LUN
-dd if=$dev1 of=$dev2
+dd if=$dev1 of=$dev2 bs=256K count=1
 
 # Verify pvs works on each device to give us vgname
-pvs --noheadings -o vg_name $dev1 1>err
-grep $vg1 err
-pvs --noheadings -o vg_name $dev2 1>err
-grep $vg1 err
+check pv_field $dev1 vg_name $vg1
+check pv_field $dev2 vg_name $vg1
 
 # Import the cloned PV to a new VG
-# FIXME: this fails on lenny buildslave, I think we need proper wrapper
-# vgimportclone --basevgname $vg2 $dev2
+vgimportclone --basevgname $vg2 $dev2
 
 # Verify we can activate / deactivate the LV from both VGs
-# lvchange -ay $vg1/$lv1
-# lvchange -ay $vg2/$lv1
-# vgchange -an $vg1
-# vgchange -an $vg2
+lvchange -ay $vg1/$lv1 $vg2/$lv1
+vgchange -an $vg1 $vg2
