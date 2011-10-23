faf7c493f866164ff101ba4cc02f7779c34858e5
Zdenek Kabelac <zkabelac@redhat.com>
2011-10-23 21:24:27 +0000

    Test with -F flag
    grep need -F to check what we really want to test.
    Add better test for existing device.
    
    Currently this test DOES NOT work with real /dev handle via udev
    since our tool does not see such device listet through udev.
    
    FIXME: We might be able to see it at least through dmsetup table and
    use for lvm.

--- test/t-mdata-strings.sh
+++ test/t-mdata-strings.sh
@@ -23,11 +23,14 @@ name=$(basename "$dev1")
 dmsetup rename "$name" "$PREFIX$pv_ugly"
 dev1=$(dirname "$dev1")/$PREFIX$pv_ugly
 
+dmsetup table | grep -F "$pv_ugly"
+
 # 'pvcreate, vgcreate on filename with backslashed chars' 
 created=$dev1
 # when used with real udev without fallback, it will fail here
 pvcreate "$dev1" || created=$dev2
-pvs | should grep $dev1
+pvdisplay | should grep -F "$pv_ugly"
+should check pv_field "$dev1" pv_name "$dev1"
 vgcreate $vg "$created"
 # 'no parse errors and VG really exists' 
 vgs 2>err
