e55220e27ac609a54b7e4090cb863bcd921f8e8f
Petr Rockai <prokai@redhat.com>
2011-11-20 21:43:20 +0000

    Implement a CUnit-based runner for unit tests. Copy and adapt (actual unit) tests from unit-tests/*/*_t.c (now under test/unit). The valgrind/pool test is missing, since it's not really a unit test and probably not too valuable either. Available via "make unit" (and if --enable-testing was passed to configure, also executed by make check).

---
+++ test/unit/matcher_t.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2001-2004 Sistina Software, Inc. All rights reserved.
+ * Copyright (C) 2004-2010 Red Hat, Inc. All rights reserved.
+ *
+ * This file is part of LVM2.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License v.2.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "libdevmapper.h"
+#include "log.h"
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include <CUnit/CUnit.h>
+#include "matcher_data.h"
+
+int regex_init(void);
+int regex_fini(void);
+
+static struct dm_pool *mem = NULL;
+
+int regex_init() {
+	mem = dm_pool_create("bitset test", 1024);
+	return mem == NULL;
+}
+
+int regex_fini() {
+	dm_pool_destroy(mem);
+	return 0;
+}
+
+static struct dm_regex *make_scanner(const char **rx)
+{
+	int nrx = 0;
+	for (; rx[nrx]; ++nrx);
+
+	struct dm_regex *scanner = dm_regex_create(mem, rx, nrx);
+	CU_ASSERT_FATAL(scanner != NULL);
+	return scanner;
+}
+
+static void test_fingerprints() {
+	struct dm_regex *scanner;
+
+	scanner = make_scanner(dev_patterns);
+	CU_ASSERT_EQUAL(dm_regex_fingerprint(scanner), 0x352b6c4f);
+
+	scanner = make_scanner(random_patterns);
+	CU_ASSERT_EQUAL(dm_regex_fingerprint(scanner), 0xeed8ceb8);
+}
+
+static void test_matching() {
+	struct dm_regex *scanner;
+	int i;
+
+	scanner = make_scanner(dev_patterns);
+	for (i = 0; devices[i].str; ++i)
+		CU_ASSERT_EQUAL(dm_regex_match(scanner, devices[i].str), devices[i].expected - 1);
+
+	scanner = make_scanner(nonprint_patterns);
+	for (i = 0; nonprint[i].str; ++i)
+		CU_ASSERT_EQUAL(dm_regex_match(scanner, nonprint[i].str), nonprint[i].expected - 1);
+}
+
+CU_TestInfo regex_list[] = {
+	{ (char*)"fingerprints", test_fingerprints },
+	{ (char*)"matching", test_matching },
+	CU_TEST_INFO_NULL
+};
+
---
+++ test/unit/run.c
@@ -0,0 +1,25 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+
+#define DECL(n) \
+	extern CU_TestInfo n ## _list[]; \
+	int n ## _init(); \
+	int n ## _fini();
+#define USE(n) { (char*) #n, n##_init, n##_fini, n##_list }
+
+DECL(bitset);
+DECL(regex);
+
+CU_SuiteInfo suites[] = {
+	USE(bitset),
+	USE(regex),
+	CU_SUITE_INFO_NULL
+};
+
+int main() {
+	CU_initialize_registry();
+	CU_register_suites(suites);
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	return CU_get_number_of_failures() != 0;
+}
