9aa1e402761e55d6597caeca78efc1cd23cd501d
Petr Rockai <prokai@redhat.com>
2011-11-21 11:25:35 +0000

    Fix the fallout in shell tests from the t-XXX -> shell/XXX move.

--- test/shell/lvconvert-mirror-basic-1.sh
+++ test/shell/lvconvert-mirror-basic-1.sh
@@ -8,5 +8,5 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-. ./t-lvconvert-mirror-basic.sh
+. ./lvconvert-mirror-basic.sh
 test_many 1
