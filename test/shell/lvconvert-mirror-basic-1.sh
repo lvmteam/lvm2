13f9043a35010ab79b264311d3bcf7c1688891bb
Petr Rockai <prokai@redhat.com>
2011-11-21 11:36:53 +0000

    Fix the last fix (t-XXX -> shell/XXX).

--- test/shell/lvconvert-mirror-basic-1.sh
+++ test/shell/lvconvert-mirror-basic-1.sh
@@ -8,5 +8,5 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-. ./lvconvert-mirror-basic.sh
+. ./shell/lvconvert-mirror-basic.sh
 test_many 1
