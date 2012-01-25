5f3191097da17bba47658badffe3a504b576890d
Zdenek Kabelac <zkabelac@redhat.com>
2012-01-25 21:31:59 +0000

    Ensure reply struct has all fields defined
    Reply is returned by value.

--- daemons/common/daemon-client.c
+++ daemons/common/daemon-client.c
@@ -34,7 +34,7 @@ error:
 
 daemon_reply daemon_send(daemon_handle h, daemon_request rq)
 {
-	daemon_reply reply;
+	daemon_reply reply = { .cft = NULL };
 	assert(h.socket_fd >= 0);
 
 	if (!rq.buffer) {
