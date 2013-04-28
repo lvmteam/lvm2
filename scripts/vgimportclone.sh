05bf4b8cc3e3d15e79c9964e496dbe396beacb3c
Petr Rockai <prockai@redhat.com>
2013-04-28 22:38:22 +0200

    vgimportclone: override global_filter in lvm.conf
    The global filter in system's lvm.conf may conflict with the custom filter we
    set up in vgimportclone (they can easily fail to intersect). Since we explicitly
    avoid talking to lvmetad in vgimportclone, it is safe and reasonable to do so.

--- scripts/vgimportclone.sh
+++ scripts/vgimportclone.sh
@@ -250,6 +250,7 @@ LVMCONF=${TMP_LVM_SYSTEM_DIR}/lvm.conf
      /^[ \t]*scan[ \t]*=/{print "scan = [ \"" DEV "\" ]";next} \
      /^[ \t]*cache[ \t]*=/{print "cache = \"" CACHE "\"";next} \
      /^[ \t]*use_lvmetad[ \t]*=/{print "use_lvmetad = 0";next} \
+     /^[ \t]*global_filter[ \t]*=/{print "global_filter = [ \"a|.*|\" ]";next} \
      /^[ \t]*cache_dir[ \t]*=/{print "cache_dir = \"" CACHE_DIR "\"";next} \
      {print $0}' > ${LVMCONF}
 
