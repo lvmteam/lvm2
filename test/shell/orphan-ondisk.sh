bd60232d7428a8b51c8794b30503761f4032f7a1
Petr Rockai <prockai@redhat.com>
2013-07-22 11:57:23 +0200

    TEST: Add a regression test for the ondisk/orphan bug.

---
+++ test/shell/orphan-ondisk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright (C) 2010 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+. lib/test
+
+aux prepare_vg 2
+vgreduce $vg $dev1 2>&1 | not grep -i 'parse error'
