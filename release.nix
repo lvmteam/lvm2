4fc075e04062dfcf4ea3e11c6b5527cf5624089e
Petr Rockai <prockai@redhat.com>
2013-08-20 17:33:38 +0200

    nix: Run udevd in foreground.

--- release.nix
+++ release.nix
@@ -164,7 +164,7 @@ let
          echo "%define check_commands \\";
          echo "make lcov-reset \\";
          echo "dmsetup targets\\";
-         echo "/usr/lib/systemd/systemd-udevd --daemon || true \\";
+         echo "/usr/lib/systemd/systemd-udevd & \\";
          echo "LVM_TEST_DEVDIR=/dev make check T=${T} || touch \$out/nix-support/failed \\"
 	 echo "cp -R test/results /tmp/test-results \\"
          echo "make lcov && cp -R lcov_reports /tmp/lcov") >> source.inc
