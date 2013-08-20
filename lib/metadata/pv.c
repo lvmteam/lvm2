67c563ac2b643f97602686dc07cee4a8bdb5bc34
Petr Rockai <prockai@redhat.com>
2013-08-21 01:30:38 +0200

    pv_label: NULL result is not always an internal error.

--- lib/metadata/pv.c
+++ lib/metadata/pv.c
@@ -355,7 +355,10 @@ struct label *pv_label(const struct physical_volume *pv)
 	struct lvmcache_info *info =
 		lvmcache_info_from_pvid((const char *)&pv->id.uuid, 0);
 	if (!info) {
-		log_error(INTERNAL_ERROR "PV %s unexpectedly not in cache.", dev_name(pv->dev));
+		if (pv->vg) /* process_each_pv will create PVs that are dummy
+			     * and that have no label associated */
+			log_error(INTERNAL_ERROR "PV %s unexpectedly not in cache.",
+				  dev_name(pv->dev));
 		return NULL;
 	}
 	return lvmcache_get_label(info);
