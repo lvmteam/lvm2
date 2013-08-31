fe4d473c5b93ff4f05b623ad841ce23be1357462
Petr Rockai <prockai@redhat.com>
2013-08-31 10:01:13 +0200

    NIX: Give the VMs more RAM (768M instead of 512M).

--- release.nix
+++ release.nix
@@ -13,7 +13,7 @@ let
      fullName = "LVM2";
      src = jobs.tarball;
      diskImage = diskFun { extraPackages = extras; };
-     memSize = 512;
+     memSize = 768;
      # fc16 lcov is broken and el6 has none... be creative
      prepareImagePhase = ''
       rpm -Uv ${pkgs.fetchurl {
