2abaf87a9b36cff2f32398a8dfb26609c0f56e14
Zdenek Kabelac <zkabelac@redhat.com>
2013-09-16 13:46:17 +0200

    tests: update vgcfgrestore test
    Test vgcfgrestore when seqno of restored mda is smaller then current
    metadata on device.

--- test/shell/vgcfgbackup-usage.sh
+++ test/shell/vgcfgbackup-usage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2008 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2008-2013 Red Hat, Inc. All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
@@ -18,9 +18,12 @@ vg1=${PREFIX}vg00
 vg2=${PREFIX}vg01
 vgcreate $vg1 "$dev1"
 vgcreate $vg2 "$dev2"
-vgcfgbackup -f $TESTDIR/bak-%s >out
+vgcfgbackup -f bak-%s >out
 grep "Volume group \"$vg1\" successfully backed up." out
 grep "Volume group \"$vg2\" successfully backed up." out
+# increase seqno
+lvcreate -l1 $vg1
+vgcfgrestore -f bak-$vg1 $vg1
 vgremove -ff $vg1 $vg2
 
 # vgcfgbackup correctly stores metadata with missing PVs
