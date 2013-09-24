3cb9041764005c3a8f59b0b5ede3808b660d001a
Zdenek Kabelac <zkabelac@redhat.com>
2013-09-24 14:47:32 +0200

    cleanup: do not pass uninitialized space to selinux.
    Just like with dm_prepare_selinux_context rather initialize to NULL.

--- libdm/libdm-common.c
+++ libdm/libdm-common.c
@@ -899,7 +899,7 @@ int dm_prepare_selinux_context(const char *path, mode_t mode)
 int dm_set_selinux_context(const char *path, mode_t mode)
 {
 #ifdef HAVE_SELINUX
-	security_context_t scontext;
+	security_context_t scontext = NULL;
 
 	if (is_selinux_enabled() <= 0)
 		return 1;
