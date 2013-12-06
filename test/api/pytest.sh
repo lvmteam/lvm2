f460d6d9b5dcf0e13b70d4c1630a0f667849b8b9
Zdenek Kabelac <zkabelac@redhat.com>
2013-12-06 10:07:22 +0100

    tests: python test needs dmeventd
    Until lvm2app respects all lvm.conf setting,
    this test require dmeventd.

--- test/api/pytest.sh
+++ test/api/pytest.sh
@@ -13,18 +13,69 @@
 
 . lib/test
 
+#
+# TODO:
+# lvm2app is not yet capable to respect many lvm.conf options
+# since a lot of them is set in /tools/liblvmline
+# Until fixed - testing always runs with enabled monitoring
+# thus it needs dmeventd
+#
+aux prepare_dmeventd
+
 #If you change this change the unit test case too.
 aux prepare_pvs 6
 
 #Locate the python binding library to use.
-python_lib=`find $abs_top_builddir -name lvm.so`
+python_lib=$(find $abs_top_builddir -name lvm.so)
 
 # Unable to test python bindings if library not available
 test -z "$python_lib" && skip
 
-export PYTHONPATH=`dirname $python_lib`:$PYTHONPATH
+export PYTHONPATH=$(dirname $python_lib):$PYTHONPATH
 
 #Setup which devices the unit test can use.
 export PY_UNIT_PVS=$(cat DEVICES)
 
-python_lvm_unit.py -v -f
+#python_lvm_unit.py -v -f
+
+# Run individual tests for shorter error trace
+python_lvm_unit.py -v TestLvm.test_config_find_bool
+python_lvm_unit.py -v TestLvm.test_config_override
+python_lvm_unit.py -v TestLvm.test_config_reload
+python_lvm_unit.py -v TestLvm.test_dupe_lv_create
+python_lvm_unit.py -v TestLvm.test_get_set_extend_size
+python_lvm_unit.py -v TestLvm.test_lv_active_inactive
+python_lvm_unit.py -v TestLvm.test_lv_property
+python_lvm_unit.py -v TestLvm.test_lv_rename
+python_lvm_unit.py -v TestLvm.test_lv_resize
+python_lvm_unit.py -v TestLvm.test_lv_seg
+python_lvm_unit.py -v TestLvm.test_lv_size
+python_lvm_unit.py -v TestLvm.test_lv_snapshot
+python_lvm_unit.py -v TestLvm.test_lv_suspend
+python_lvm_unit.py -v TestLvm.test_lv_tags
+python_lvm_unit.py -v TestLvm.test_percent_to_float
+python_lvm_unit.py -v TestLvm.test_pv_create
+python_lvm_unit.py -v TestLvm.test_pv_empty_listing
+python_lvm_unit.py -v TestLvm.test_pv_getters
+python_lvm_unit.py -v TestLvm.test_pv_life_cycle
+python_lvm_unit.py -v TestLvm.test_pv_lookup_from_vg
+python_lvm_unit.py -v TestLvm.test_pv_property
+python_lvm_unit.py -v TestLvm.test_pv_resize
+python_lvm_unit.py -v TestLvm.test_pv_segs
+python_lvm_unit.py -v TestLvm.test_scan
+python_lvm_unit.py -v TestLvm.test_version
+python_lvm_unit.py -v TestLvm.test_vg_from_pv_lookups
+python_lvm_unit.py -v TestLvm.test_vg_getters
+python_lvm_unit.py -v TestLvm.test_vg_get_name
+python_lvm_unit.py -v TestLvm.test_vg_get_set_prop
+python_lvm_unit.py -v TestLvm.test_vg_get_uuid
+python_lvm_unit.py -v TestLvm.test_vg_lv_name_validate
+python_lvm_unit.py -v TestLvm.test_vg_names
+python_lvm_unit.py -v TestLvm.test_vg_reduce
+python_lvm_unit.py -v TestLvm.test_vg_remove_restore
+python_lvm_unit.py -v TestLvm.test_vg_tags
+python_lvm_unit.py -v TestLvm.test_vg_uuids
+
+# CHECKME: not for testing?
+#python_lvm_unit.py -v TestLvm.test_listing
+#python_lvm_unit.py -v TestLvm.test_pv_methods
