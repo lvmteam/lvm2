1937715d2ccdd46122da862c6d7c1ef0ed7d1230
Petr Rockai <prockai@redhat.com>
2014-01-08 15:23:30 +0100

    test: Add a regression test for pvcreate -ff.

---
+++ test/shell/pvcreate-ff.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Copyright (C) 2014 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+. lib/test
+
+aux prepare_devs 2
+pvcreate $dev1
+vgcreate foo $dev1
+pvcreate -ff -y $dev1
+vgs
+vgcreate foo $dev1
