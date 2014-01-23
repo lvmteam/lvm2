2c99601267072a6d9b8e418f860b7b6726eb9561
Petr Rockai <prockai@redhat.com>
2014-01-23 16:42:54 +0100

    NIX: Add a local nix-build.sh script.

---
+++ nix-build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+rm -f result
+rm -f divine-snapshot.tar.gz
+rm -rf lvm-snapshot
+mkdir lvm-snapshot
+git ls-tree -r HEAD --name-only | xargs cp --parents --target-directory=lvm-snapshot
+tar cvzf lvm-snapshot.tar.gz lvm-snapshot
+nix-build release.nix \
+    --arg lvm2Src "`pwd`/lvm-snapshot.tar.gz" \
+    --arg lvm2Nix `pwd` -A "$@"
