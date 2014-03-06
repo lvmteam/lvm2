6b561a7a3b0a9dde9bba139154ebc881e89542e5
Zdenek Kabelac <zkabelac@redhat.com>
2014-03-06 17:19:35 +0100

    tests: check created snapshot has good max size
    Testing if we are creating large enough old-snapshot
    (so the COW max size is correct)
    https://bugzilla.redhat.com/show_bug.cgi?id=1035871

---
+++ test/shell/snapshot-maxsize.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Copyright (C) 2014 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Testing calculation of snapshot space
+# https://bugzilla.redhat.com/show_bug.cgi?id=1035871
+
+. lib/test
+
+aux prepare_pvs 1
+
+vgcreate -s 1K $vg $(cat DEVICES)
+
+lvcreate -L1 -n $lv1 $vg
+# Snapshot should be large enough to handle any writes
+lvcreate -L2 -s $vg/$lv1 -n $lv2
+
+dd if=/dev/zero of="$DM_DEV_DIR/$vg/$lv2" bs=1M count=1
+
+get lv_field $vg/$lv2 lv_attr | tee attrs
+# Snapshot must not be invalid here
+grep -v "....I" attrs
+
+vgremove -f $vg
