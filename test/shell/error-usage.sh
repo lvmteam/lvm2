e398901ed1d77684155a52c464f6265f68a96562
Zdenek Kabelac <zkabelac@redhat.com>
2014-03-17 13:16:17 +0100

    tests: zero and error type tests

---
+++ test/shell/error-usage.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Copyright (C) 2014 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Basic usage of zero target
+
+. lib/test
+
+which md5sum || skip
+
+aux prepare_pvs 1
+
+vgcreate -s 256k $vg $(cat DEVICES)
+
+lvcreate --type error -L1 -n $lv1 $vg
+lvextend -L+1 $vg/$lv1
+
+# has to match
+
+check lv_field $vg/$lv1 lv_modules "error"
+check lv_field $vg/$lv1 segtype "error"
+check lv_field $vg/$lv1 seg_count "1"
+check lv_field $vg/$lv1 seg_size_pe "8"   # 8 * 256
+
+# FIXME should we print info we are ignoring stripping?
+lvextend -L+1 -I64 -i2 $vg/$lv1
---
+++ test/shell/zero-usage.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright (C) 2014 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Basic usage of zero target
+
+. lib/test
+
+which md5sum || skip
+
+aux prepare_pvs 1
+
+vgcreate -s 256k $vg $(cat DEVICES)
+
+lvcreate --type zero -L1 -n $lv1 $vg
+lvextend -L+1 $vg/$lv1
+
+sum1=$(dd if=/dev/zero bs=2M count=1 | md5sum | cut -f1 -d' ')
+sum2=$(dd if="$DM_DEV_DIR/$vg/$lv1" bs=2M count=1 | md5sum | cut -f1 -d' ')
+
+# has to match
+test "$sum1" = "$sum2"
+
+check lv_field $vg/$lv1 lv_modules "zero"
+check lv_field $vg/$lv1 segtype "zero"
+check lv_field $vg/$lv1 seg_count "1"
+check lv_field $vg/$lv1 seg_size_pe "8"   # 8 * 256
