8471fb01d885aab5c46b61602352ca90814f8a8e
Zdenek Kabelac <zkabelac@redhat.com>
2014-03-25 09:59:43 +0100

    tests: wipe signature
    Check for usable blkid.

--- test/shell/lvcreate-signature-wiping.sh
+++ test/shell/lvcreate-signature-wiping.sh
@@ -13,81 +13,101 @@
 
 . lib/test
 
+init_lv_() {
+	mkswap "$DM_DEV_DIR/$vg/$lv1"
+}
+
+test_blkid_() {
+	local type=$(blkid -s TYPE -o value -c /dev/null "$DM_DEV_DIR/$vg/$lv1")
+	test "$type" = "swap"
+}
+
+test_msg_() {
+	grep "Wiping swap signature" out
+}
+
 aux prepare_vg
 
 # lvcreate wipes signatures when found on newly created LV - test this on "swap".
 # Test all combinatios with -Z{y|n} and -W{y|n} and related lvm.conf settings.
 
 lvcreate -l1 -n $lv1 $vg
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+init_lv_
+# This system has unusable blkid (does not recognize small swap, needs fix...)
+test_blkid_ || skip
 lvremove -f $vg/$lv1
-
-wiping_msg="Wiping swap signature"
 
 aux lvmconf "allocation/wipe_signatures_when_zeroing_new_lvs = 0"
 
-lvcreate -y -Zn -l1 -n $lv1 $vg | not grep "$wiping_msg"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zn -l1 -n $lv1 $vg 2>&1 | tee out
+not test_msg_
+test_blkid_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zn -Wn -l1 -n $lv1 $vg | not grep "$wiping_msg"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zn -Wn -l1 -n $lv1 $vg 2>&1 | tee out
+not test_msg_
+test_blkid_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zn -Wy -l1 -n $lv1 $vg | grep "$wiping_msg"
-(blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" || true) | not grep "swap"
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zn -Wy -l1 -n $lv1 $vg 2>&1 | tee out
+test_msg_
+not test_blkid_
+init_lv_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zy -l1 -n $lv1 $vg | not grep "$wiping_msg"
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zy -l1 -n $lv1 $vg 2>&1 | tee out
+not test_msg_
+not test_blkid_
+init_lv_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zy -Wn -l1 -n $lv1 $vg | not grep "$wiping_msg"
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zy -Wn -l1 -n $lv1 $vg 2>&1 | tee out
+not test_msg_
+not test_blkid_
+init_lv_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zy -Wy -l1 -n $lv1 $vg | grep "$wiping_msg"
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zy -Wy -l1 -n $lv1 $vg 2>&1 | tee out
+test_msg_
+not test_blkid_
+init_lv_
 lvremove -f $vg/$lv1
 
 
 aux lvmconf "allocation/wipe_signatures_when_zeroing_new_lvs = 1"
 
-lvcreate -y -Zn -l1 -n $lv1 $vg | not grep "$wiping_msg"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zn -l1 -n $lv1 $vg 2>&1 | tee out
+not test_msg_
+test_blkid_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zn -Wn -l1 -n $lv1 $vg | not grep "$wiping_msg"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zn -Wn -l1 -n $lv1 $vg 2>&1 | tee out
+not test_msg_
+test_blkid_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zn -Wy -l1 -n $lv1 $vg | grep "$wiping_msg"
-(blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" || true) | not grep "swap"
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zn -Wy -l1 -n $lv1 $vg 2>&1 | tee out
+test_msg_
+not test_blkid_
+init_lv_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zy -l1 -n $lv1 $vg | grep "$wiping_msg"
-(blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" || true) | not grep "swap"
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zy -l1 -n $lv1 $vg 2>&1 | tee out
+test_msg_
+not test_blkid_
+init_lv_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zy -Wn -l1 -n $lv1 $vg | not grep "$wiping_msg"
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zy -Wn -l1 -n $lv1 $vg 2>&1 | tee out
+not test_msg_
+not test_blkid_
+init_lv_
 lvremove -f $vg/$lv1
 
-lvcreate -y -Zy -Wy -l1 -n $lv1 $vg | grep "$wiping_msg"
-(blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" || true) | not grep "swap"
-mkswap "$DM_DEV_DIR/$vg/$lv1"
-blkid -c /dev/null "$DM_DEV_DIR/$vg/$lv1" | grep "swap"
+lvcreate -y -Zy -Wy -l1 -n $lv1 $vg 2>&1 | tee out
+test_msg_
+not test_blkid_
+init_lv_
 lvremove -f $vg/$lv1
 
 vgremove -f $vg
