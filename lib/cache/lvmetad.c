lvmteam/lvm2

commit 732859d21f3b41bdb188f92b60f25d5c94dcee8a

refactor: rename embedding area -> bootloader area

WHATS_NEW
@@ -38,9 +38,9 @@ Version 2.02.99 -
   Add --withcomments and --withversions switch to lvm dumpconfig.
   Add --type {current|default|missing|new} and --atversion to lvm dumpconfig.
   Support automatic config validation and add 'config' section to lvm.conf.
-  Add pvs -o ea_start,ea_size to report embedding area start and size.
-  Add --embeddingareasize to pvcreate and vgconvert to create an embedding area.
-  Add PV header extension: extension version, flags and embedding areas.
+  Add pvs -o ea_start,ea_size to report bootloader area start and size.
+  Add --bootloaderareasize to pvcreate and vgconvert to create a bootloader area.
+  Add PV header extension: extension version, flags and bootloader areas.
   Initial support for lvconvert of thin external origin.
   Add _lv_remove_segs_using_this_lv() for removal of dependent lvs.
   Improve activation code for better support of stacked devices.

lib/cache/lvmetad.c
@@ -280,7 +280,7 @@ static struct lvmcache_info *_pv_populate_lvmcache(
 	lvmcache_set_device_size(info, devsize);
 	lvmcache_del_das(info);
 	lvmcache_del_mdas(info);
-	lvmcache_del_eas(info);
+	lvmcache_del_bas(info);
 
 	do {
 		sprintf(mda_id, "mda%d", i);
@@ -309,7 +309,7 @@ static struct lvmcache_info *_pv_populate_lvmcache(
 		if (da) {
 			if (!dm_config_get_uint64(da->child, "offset", &offset)) return_0;
 			if (!dm_config_get_uint64(da->child, "size", &size)) return_0;
-			lvmcache_add_ea(info, offset, size);
+			lvmcache_add_ba(info, offset, size);
 		}
 		++i;
 	} while (da);
@@ -706,9 +706,9 @@ static int _extract_da(struct disk_locn *da, void *baton)
 	return _extract_disk_location("da", da, baton);
 }
 
-static int _extract_ea(struct disk_locn *ea, void *baton)
+static int _extract_ba(struct disk_locn *ba, void *baton)
 {
-	return _extract_disk_location("ea", ea, baton);
+	return _extract_disk_location("ba", ba, baton);
 }
 
 static int _extract_mdas(struct lvmcache_info *info, struct dm_config_tree *cft,
@@ -722,7 +722,7 @@ static int _extract_mdas(struct lvmcache_info *info, struct dm_config_tree *cft,
 	if (!lvmcache_foreach_da(info, &_extract_da, &baton))
 		return 0;
 	baton.i = 0;
-	if (!lvmcache_foreach_ea(info, &_extract_ea, &baton))
+	if (!lvmcache_foreach_ba(info, &_extract_ba, &baton))
 		return 0;
 
 	return 1;

lib/cache/lvmcache.c
@@ -38,7 +38,7 @@ struct lvmcache_info {
 	struct dm_list list;	/* Join VG members together */
 	struct dm_list mdas;	/* list head for metadata areas */
 	struct dm_list das;	/* list head for data areas */
-	struct dm_list eas;	/* list head for embedding areas */
+	struct dm_list bas;	/* list head for bootloader areas */
 	struct lvmcache_vginfo *vginfo;	/* NULL == unknown */
 	struct label *label;
 	const struct format_type *fmt;
@@ -1479,7 +1479,7 @@ struct lvmcache_info *lvmcache_add(struct labeller *labeller, const char *pvid,
 
 		lvmcache_del_mdas(info);
 		lvmcache_del_das(info);
-		lvmcache_del_eas(info);
+		lvmcache_del_bas(info);
 	} else {
 		if (existing->dev != dev) {
 			/* Is the existing entry a duplicate pvid e.g. md ? */
@@ -1724,19 +1724,19 @@ int lvmcache_populate_pv_fields(struct lvmcache_info *info,
 		return 0;
 	}
 
-	/* Currently only support one embedding area at most */
-	if (dm_list_size(&info->eas) > 1) {
-		log_error("Must be at most one embedding area (found %d) on PV %s",
-			  dm_list_size(&info->eas), dev_name(info->dev));
+	/* Currently only support one bootloader area at most */
+	if (dm_list_size(&info->bas) > 1) {
+		log_error("Must be at most one bootloader area (found %d) on PV %s",
+			  dm_list_size(&info->bas), dev_name(info->dev));
 		return 0;
 	}
 
 	dm_list_iterate_items(da, &info->das)
 		pv->pe_start = da->disk_locn.offset >> SECTOR_SHIFT;
 
-	dm_list_iterate_items(da, &info->eas) {
-		pv->ea_start = da->disk_locn.offset >> SECTOR_SHIFT;
-		pv->ea_size = da->disk_locn.size >> SECTOR_SHIFT;
+	dm_list_iterate_items(da, &info->bas) {
+		pv->ba_start = da->disk_locn.offset >> SECTOR_SHIFT;
+		pv->ba_size = da->disk_locn.size >> SECTOR_SHIFT;
 	}
 
 	return 1;
@@ -1766,11 +1766,11 @@ void lvmcache_del_das(struct lvmcache_info *info)
 	dm_list_init(&info->das);
 }
 
-void lvmcache_del_eas(struct lvmcache_info *info)
+void lvmcache_del_bas(struct lvmcache_info *info)
 {
-	if (info->eas.n)
-		del_eas(&info->eas);
-	dm_list_init(&info->eas);
+	if (info->bas.n)
+		del_bas(&info->bas);
+	dm_list_init(&info->bas);
 }
 
 int lvmcache_add_mda(struct lvmcache_info *info, struct device *dev,
@@ -1784,9 +1784,9 @@ int lvmcache_add_da(struct lvmcache_info *info, uint64_t start, uint64_t size)
 	return add_da(NULL, &info->das, start, size);
 }
 
-int lvmcache_add_ea(struct lvmcache_info *info, uint64_t start, uint64_t size)
+int lvmcache_add_ba(struct lvmcache_info *info, uint64_t start, uint64_t size)
 {
-	return add_ea(NULL, &info->eas, start, size);
+	return add_ba(NULL, &info->bas, start, size);
 }
 
 void lvmcache_update_pv(struct lvmcache_info *info, struct physical_volume *pv,
@@ -1813,20 +1813,20 @@ int lvmcache_update_das(struct lvmcache_info *info, struct physical_volume *pv)
 	return 1;
 }
 
-int lvmcache_update_eas(struct lvmcache_info *info, struct physical_volume *pv)
+int lvmcache_update_bas(struct lvmcache_info *info, struct physical_volume *pv)
 {
-	struct data_area_list *ea;
-	if (info->eas.n) {
-		if (!pv->ea_start && !pv->ea_size)
-			dm_list_iterate_items(ea, &info->eas) {
-				pv->ea_start = ea->disk_locn.offset >> SECTOR_SHIFT;
-				pv->ea_size = ea->disk_locn.size >> SECTOR_SHIFT;
+	struct data_area_list *ba;
+	if (info->bas.n) {
+		if (!pv->ba_start && !pv->ba_size)
+			dm_list_iterate_items(ba, &info->bas) {
+				pv->ba_start = ba->disk_locn.offset >> SECTOR_SHIFT;
+				pv->ba_size = ba->disk_locn.size >> SECTOR_SHIFT;
 			}
-		del_das(&info->eas);
+		del_das(&info->bas);
 	} else
-		dm_list_init(&info->eas);
+		dm_list_init(&info->bas);
 
-	if (!add_ea(NULL, &info->eas, pv->ea_start << SECTOR_SHIFT, pv->ea_size << SECTOR_SHIFT))
+	if (!add_ba(NULL, &info->bas, pv->ba_start << SECTOR_SHIFT, pv->ba_size << SECTOR_SHIFT))
 		return_0;
 
 	return 1;
@@ -1876,13 +1876,13 @@ int lvmcache_foreach_da(struct lvmcache_info *info,
 	return 1;
 }
 
-int lvmcache_foreach_ea(struct lvmcache_info *info,
+int lvmcache_foreach_ba(struct lvmcache_info *info,
 			 int (*fun)(struct disk_locn *, void *),
 			 void *baton)
 {
-	struct data_area_list *ea;
-	dm_list_iterate_items(ea, &info->eas) {
-		if (!fun(&ea->disk_locn, baton))
+	struct data_area_list *ba;
+	dm_list_iterate_items(ba, &info->bas) {
+		if (!fun(&ba->disk_locn, baton))
 			return_0;
 	}
 

lib/cache/lvmcache.h
@@ -118,11 +118,11 @@ int lvmcache_populate_pv_fields(struct lvmcache_info *info,
 int lvmcache_check_format(struct lvmcache_info *info, const struct format_type *fmt);
 void lvmcache_del_mdas(struct lvmcache_info *info);
 void lvmcache_del_das(struct lvmcache_info *info);
-void lvmcache_del_eas(struct lvmcache_info *info);
+void lvmcache_del_bas(struct lvmcache_info *info);
 int lvmcache_add_mda(struct lvmcache_info *info, struct device *dev,
 		     uint64_t start, uint64_t size, unsigned ignored);
 int lvmcache_add_da(struct lvmcache_info *info, uint64_t start, uint64_t size);
-int lvmcache_add_ea(struct lvmcache_info *info, uint64_t start, uint64_t size);
+int lvmcache_add_ba(struct lvmcache_info *info, uint64_t start, uint64_t size);
 
 const struct format_type *lvmcache_fmt(struct lvmcache_info *info);
 struct label *lvmcache_get_label(struct lvmcache_info *info);
@@ -130,7 +130,7 @@ struct label *lvmcache_get_label(struct lvmcache_info *info);
 void lvmcache_update_pv(struct lvmcache_info *info, struct physical_volume *pv,
 			const struct format_type *fmt);
 int lvmcache_update_das(struct lvmcache_info *info, struct physical_volume *pv);
-int lvmcache_update_eas(struct lvmcache_info *info, struct physical_volume *pv);
+int lvmcache_update_bas(struct lvmcache_info *info, struct physical_volume *pv);
 int lvmcache_foreach_mda(struct lvmcache_info *info,
 			 int (*fun)(struct metadata_area *, void *),
 			 void *baton);
@@ -139,7 +139,7 @@ int lvmcache_foreach_da(struct lvmcache_info *info,
 			int (*fun)(struct disk_locn *, void *),
 			void *baton);
 
-int lvmcache_foreach_ea(struct lvmcache_info *info,
+int lvmcache_foreach_ba(struct lvmcache_info *info,
 			int (*fun)(struct disk_locn *, void *),
 			void *baton);
 

lib/format1/format1.c
@@ -384,8 +384,8 @@ static int _format1_pv_setup(const struct format_type *fmt,
 	struct pvcreate_restorable_params rp = {.restorefile = NULL,
 						.id = {{0}},
 						.idp = NULL,
-						.ea_start = 0,
-						.ea_size = 0,
+						.ba_start = 0,
+						.ba_size = 0,
 						.pe_start = 0,
 						.extent_count = 0,
 						.extent_size = vg->extent_size};
@@ -430,7 +430,7 @@ static int _format1_pv_write(const struct format_type *fmt, struct physical_volu
 	lvmcache_update_pv(info, pv, fmt);
 	lvmcache_del_mdas(info);
 	lvmcache_del_das(info);
-	lvmcache_del_eas(info);
+	lvmcache_del_bas(info);
 
 	dm_list_init(&pvs);
 

lib/format_text/export.c
@@ -490,9 +490,9 @@ static int _print_pvs(struct formatter *f, struct volume_group *vg)
 		outsize(f, vg->extent_size * (uint64_t) pv->pe_count,
 			"pe_count = %u", pv->pe_count);
 
-		if (pv->ea_start && pv->ea_size) {
-			outf(f, "ea_start = %" PRIu64, pv->ea_start);
-			outsize(f, pv->ea_size, "ea_size = %" PRIu64, pv->ea_size);
+		if (pv->ba_start && pv->ba_size) {
+			outf(f, "ba_start = %" PRIu64, pv->ba_start);
+			outsize(f, pv->ba_size, "ba_size = %" PRIu64, pv->ba_size);
 		}
 
 		_dec_indent(f);

lib/format_text/format-text.c
@@ -1331,7 +1331,7 @@ static int _text_pv_write(const struct format_type *fmt, struct physical_volume 
 				 mdac->area.start, mdac->area.size, mda_is_ignored(mda));
 	}
 
-	if (!lvmcache_update_eas(info, pv))
+	if (!lvmcache_update_bas(info, pv))
 		return_0;
 
 	/*
@@ -1510,15 +1510,15 @@ static int _text_pv_initialise(const struct format_type *fmt,
 		return 0;
 	}
 
-	if (pv->size < final_alignment + rp->ea_size) {
-		log_error("%s: Embedding area with data-aligned start must "
+	if (pv->size < final_alignment + rp->ba_size) {
+		log_error("%s: Bootloader area with data-aligned start must "
 			  "not exceed device size.", pv_dev_name(pv));
 		return 0;
 	}
 
 	if (rp->pe_start == PV_PE_START_CALC) {
 		/*
-		 * Calculate new PE start and embedding area start value.
+		 * Calculate new PE start and bootloader area start value.
 		 * Make sure both are properly aligned!
 		 * If PE start can't be aligned because EA is taking
 		 * the whole space, make PE start equal to the PV size
@@ -1526,14 +1526,14 @@ static int _text_pv_initialise(const struct format_type *fmt,
 		 * This needs to be done as we can't have a PV without any DA.
 		 * But we still want to support a PV with EA only!
 		 */
-		if (rp->ea_size) {
-			pv->ea_start = final_alignment;
-			pv->ea_size = rp->ea_size;
-			if ((adjustment = rp->ea_size % pv->pe_align))
-				pv->ea_size += pv->pe_align - adjustment;
-			if (pv->size < pv->ea_start + pv->ea_size)
-				pv->ea_size = pv->size - pv->ea_start;
-			pv->pe_start = pv->ea_start + pv->ea_size;
+		if (rp->ba_size) {
+			pv->ba_start = final_alignment;
+			pv->ba_size = rp->ba_size;
+			if ((adjustment = rp->ba_size % pv->pe_align))
+				pv->ba_size += pv->pe_align - adjustment;
+			if (pv->size < pv->ba_start + pv->ba_size)
+				pv->ba_size = pv->size - pv->ba_start;
+			pv->pe_start = pv->ba_start + pv->ba_size;
 		} else
 			pv->pe_start = final_alignment;
 	} else {
@@ -1548,16 +1548,16 @@ static int _text_pv_initialise(const struct format_type *fmt,
 		 *       in MDA then!
 		 */
 		pv->pe_start = rp->pe_start;
-		if (rp->ea_size) {
-			if ((rp->ea_start && rp->ea_start + rp->ea_size > rp->pe_start) ||
+		if (rp->ba_size) {
+			if ((rp->ba_start && rp->ba_start + rp->ba_size > rp->pe_start) ||
 			    (rp->pe_start <= final_alignment) ||
-			    (rp->pe_start - final_alignment < rp->ea_size)) {
-				log_error("%s: Embedding area would overlap "
+			    (rp->pe_start - final_alignment < rp->ba_size)) {
+				log_error("%s: Bootloader area would overlap "
 					  "data area.", pv_dev_name(pv));
 				return 0;
 			} else {
-				pv->ea_start = rp->ea_start ? : final_alignment;
-				pv->ea_size = rp->ea_size;
+				pv->ba_start = rp->ba_start ? : final_alignment;
+				pv->ba_size = rp->ba_size;
 			}
 		}
 	}
@@ -1985,7 +1985,7 @@ static int _text_pv_add_metadata_area(const struct format_type *fmt,
 {
 	struct format_instance *fid = pv->fid;
 	const char *pvid = (const char *) (*pv->old_id.uuid ? &pv->old_id : &pv->id);
-	uint64_t ea_size, pe_start, pe_end;
+	uint64_t ba_size, pe_start, pe_end;
 	uint64_t alignment, alignment_offset;
 	uint64_t disk_size;
 	uint64_t mda_start;
@@ -2006,7 +2006,7 @@ static int _text_pv_add_metadata_area(const struct format_type *fmt,
 	}
 
 	pe_start = pv->pe_start << SECTOR_SHIFT;
-	ea_size = pv->ea_size << SECTOR_SHIFT;
+	ba_size = pv->ba_size << SECTOR_SHIFT;
 	alignment = pv->pe_align << SECTOR_SHIFT;
 	alignment_offset = pv->pe_align_offset << SECTOR_SHIFT;
 	disk_size = pv->size << SECTOR_SHIFT;
@@ -2042,10 +2042,10 @@ static int _text_pv_add_metadata_area(const struct format_type *fmt,
 			limit_name = "disk size";
 		}
 
-		/* Adjust limits for embedding area if present. */
-		if (ea_size) {
-			limit -= ea_size;
-			limit_name = "ea_start";
+		/* Adjust limits for bootloader area if present. */
+		if (ba_size) {
+			limit -= ba_size;
+			limit_name = "ba_start";
 		}
 
 		if (limit > disk_size)
@@ -2107,9 +2107,9 @@ static int _text_pv_add_metadata_area(const struct format_type *fmt,
 		 * start of the area that follows the MDA0 we've just calculated.
 		 */
 		if (!pe_start_locked) {
-			if (ea_size) {
-				pv->ea_start = (mda_start + mda_size) >> SECTOR_SHIFT;
-				pv->pe_start = pv->ea_start + pv->ea_size;
+			if (ba_size) {
+				pv->ba_start = (mda_start + mda_size) >> SECTOR_SHIFT;
+				pv->pe_start = pv->ba_start + pv->ba_size;
 			} else
 				pv->pe_start = (mda_start + mda_size) >> SECTOR_SHIFT;
 		}
@@ -2140,10 +2140,10 @@ static int _text_pv_add_metadata_area(const struct format_type *fmt,
 				limit_name = "label scan size";
 			}
 
-			/* Adjust limits for embedding area if present. */
-			if (ea_size) {
-				limit += ea_size;
-				limit_name = "ea_end";
+			/* Adjust limits for bootloader area if present. */
+			if (ba_size) {
+				limit += ba_size;
+				limit_name = "ba_end";
 			}
 		}
 
@@ -2413,7 +2413,7 @@ struct format_type *create_text_format(struct cmd_context *cmd)
 	fmt->orphan_vg_name = ORPHAN_VG_NAME(FMT_TEXT_NAME);
 	fmt->features = FMT_SEGMENTS | FMT_MDAS | FMT_TAGS | FMT_PRECOMMIT |
 			FMT_UNLIMITED_VOLS | FMT_RESIZE_PV |
-			FMT_UNLIMITED_STRIPESIZE | FMT_EAS;
+			FMT_UNLIMITED_STRIPESIZE | FMT_BAS;
 
 	if (!(mda_lists = dm_malloc(sizeof(struct mda_lists)))) {
 		log_error("Failed to allocate dir_list");

lib/format_text/format-text.h
@@ -58,9 +58,9 @@ int pvhdr_read(struct device *dev, char *buf);
 int add_da(struct dm_pool *mem, struct dm_list *das,
 	   uint64_t start, uint64_t size);
 void del_das(struct dm_list *das);
-int add_ea(struct dm_pool *mem, struct dm_list *eas,
+int add_ba(struct dm_pool *mem, struct dm_list *eas,
 	   uint64_t start, uint64_t size);
-void del_eas(struct dm_list *eas);
+void del_bas(struct dm_list *eas);
 int add_mda(const struct format_type *fmt, struct dm_pool *mem, struct dm_list *mdas,
 	    struct device *dev, uint64_t start, uint64_t size, unsigned ignored);
 void del_mdas(struct dm_list *mdas);

commit 10e10cfa4fd802ffe404c0afb30ad896003c9437

lvmetad: fix lost bootloader area information
The refactoring made by 732859d21f3b41bdb188f92b60f25d5c94dcee8a
caused this. The former "ea" was not renamed to "ba" and we used
incorrect tree node name to search for the value.

WHATS_NEW
@@ -1,5 +1,6 @@
 Version 2.02.106 - 
 ====================================
+  Fix lost information about bootloader area when using lvmetad.
   Don't require --major to be specified when using -My option on kernels > 2.4.
   Add configure --disable-thin_check_needs_check to support old thin_check.
   Use thin_check --clear-needs-check-flag by default.

lib/cache/lvmetad.c
@@ -349,7 +349,7 @@ static struct lvmcache_info *_pv_populate_lvmcache(struct cmd_context *cmd,
 
 	i = 0;
 	do {
-		sprintf(da_id, "ea%d", i);
+		sprintf(da_id, "ba%d", i);
 		da = dm_config_find_node(cn->child, da_id);
 		if (da) {
 			if (!dm_config_get_uint64(da->child, "offset", &offset)) return_0;