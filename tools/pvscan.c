e8a3ba18659404b4ebded504a37623eeae8cc830
Alasdair G Kergon <agk@redhat.com>
2014-04-18 02:13:46 +0100

    pvscan: Use lvmetad_used().
    Config variables that are processed during setup prior to calling into
    particular tools must not be accessed directly afterwards in case the
    values already got overridden.
    
    _process_config() already used the tests I'm removing here to call
    lvmetad_set_active() and set up lvmetad_used().

--- tools/pvscan.c
+++ tools/pvscan.c
@@ -201,9 +201,8 @@ static int _pvscan_lvmetad(struct cmd_context *cmd, int argc, char **argv)
 	 * and to prevent hangs in clustered environment.
 	 */
 	/* TODO: Remove this once lvmetad + cluster supported! */
-	if (find_config_tree_int(cmd, global_locking_type_CFG, NULL) == 3 ||
-	    !find_config_tree_bool(cmd, global_use_lvmetad_CFG, NULL)) {
-		log_debug_lvmetad("_pvscan_lvmetad: immediate return");
+	if (!lvmetad_used()) {
+		log_verbose("Ignoring pvscan --cache command because lvmetad is not in use.");
 		return ret;
 	}
 
