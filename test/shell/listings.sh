26989e0cd7fab4768f46803cd25bfc7499ab488e
Zdenek Kabelac <zkabelac@redhat.com>
2014-04-28 11:57:42 +0200

    tests: improve coverage
    Test more code paths for lvscan & lvdisplay

--- test/shell/listings.sh
+++ test/shell/listings.sh
@@ -61,10 +61,19 @@ dmsetup ls | grep "$PREFIX" | grep -v "LVMTEST.*pv."
 lvremove -f $vg/$lv2
 
 #COMM lvs -a displays mirror legs and log
-lvcreate -aey -l4 --type mirror -m2 -n $lv3 $vg
+lvcreate -aey -l2 --type mirror -m2 -n $lv3 $vg
 test $(lvs --noheadings $vg | wc -l) -eq 2
 test $(lvs -a --noheadings $vg | wc -l) -eq 6
 dmsetup ls|grep "$PREFIX"|grep -v "LVMTEST.*pv."
+lvcreate -l2 -s $vg/$lv3
+lvcreate -l1 -s -n inval $vg/$lv3
+lvcreate -l4 -I4 -i2 -n stripe $vg
+lvcreate -T $vg/pool -I4 -i2 -l5 -V1P -n thin
+# Invalidate snapshot
+not dd if=/dev/zero of="$DM_DEV_DIR/$vg/inval" bs=4K
+invalid lvscan "$dev1"
+lvdisplay --maps
+not lvscan --all
 
 #COMM vgs with options from pvs still treats arguments as VGs (bz193543)
 vgs -o pv_name,vg_name $vg
