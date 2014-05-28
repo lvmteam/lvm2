8212dac849e5a820c579c06a1b2805e7934a8d8b
Zdenek Kabelac <zkabelac@redhat.com>
2014-05-28 15:41:06 +0200

    tests: rename test

--- test/shell/pv-duplicate-uuid.sh
+++ test/shell/pv-duplicate-uuid.sh
@@ -9,6 +9,7 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+# Test 'Found duplicate' is shown
 . lib/test
 
 aux prepare_devs 3
