70551eec5921e3b8bce7318880e54836f467b5fa
Zdenek Kabelac <zkabelac@redhat.com>
2014-06-30 14:04:25 +0200

    uuid: revert uuids for mirrors and raids
    Using suffixes for mirrors and raids will need more work,
    before this could be enabled.
    
    Meanwhile revert to previous behavior.
    
    Keep suffixes for thins and caches.

--- lib/misc/lvm-string.c
+++ lib/misc/lvm-string.c
@@ -182,15 +182,16 @@ char *build_dm_uuid(struct dm_pool *mem, const struct logical_volume *lv,
 		/*
 		 * Mark internal LVs with layer suffix
 		 * so tools like blkid may immeditelly see it's
-		 * an internal LV they should not scan
-                 * Should also make internal detection simpler
+		 * an internal LV they should not scan.
+		 * Should also make internal detection simpler.
 		 */
 		layer = lv_is_cache_pool_data(lv) ? "cdata" :
 			lv_is_cache_pool_metadata(lv) ? "cmeta" :
-			lv_is_mirror_image(lv) ? "mimage" :
-			lv_is_mirror_log(lv) ? "mlog" :
-			lv_is_raid_image(lv) ? "rimage" :
-			lv_is_raid_metadata(lv) ? "rmeta" :
+			// FIXME: dm-tree needs fixes for mirrors/raids
+			//lv_is_mirror_image(lv) ? "mimage" :
+			//lv_is_mirror_log(lv) ? "mlog" :
+			//lv_is_raid_image(lv) ? "rimage" :
+			//lv_is_raid_metadata(lv) ? "rmeta" :
 			lv_is_thin_pool(lv) ? "pool" :
 			lv_is_thin_pool_data(lv) ? "tdata" :
 			lv_is_thin_pool_metadata(lv) ? "tmeta" :
