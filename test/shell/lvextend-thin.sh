c4484d90503c4e5c2bc945461da50a6b85c5d7e7
Petr Rockai <prockai@redhat.com>
2014-07-30 16:15:16 +0200

    test: Add a test for lvextend -l+100%FREE of a striped thin pool.

---
+++ test/shell/lvextend-thin.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Copyright (C) 2014 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+. lib/inittest
+
+aux prepare_vg 3
+lvcreate -i2 -l2 -T $vg/pool2
+lvextend -l+2 $vg/pool2 $dev2 $dev3
+should lvextend -l+100%FREE $vg/pool2
