45e2aee8f9173cc2858da93a4bb9bc289a36bf9d
Petr Rockai <prockai@redhat.com>
2014-10-01 11:53:24 +0200

    test: It is not an error for a test daemon to exit before teardown.

--- test/lib/aux.sh
+++ test/lib/aux.sh
@@ -214,7 +214,7 @@ kill_sleep_kill_() {
 	slow=$2
 	if test -s $pidfile ; then
 		pid=$(< $pidfile)
-		kill -TERM $pid
+		kill -TERM $pid || return 0
 		if test $slow -eq 0 ; then sleep .1 ; else sleep 1 ; fi
 		kill -KILL $pid 2>/dev/null || true
 		wait=0
