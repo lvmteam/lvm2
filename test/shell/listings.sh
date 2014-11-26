lvmteam/lvm2

commit b0dde9e8f026ddd6799d6ca534ff9523d633c735

toollib: refactor skip_dev_dir
Detect dev dir just once.

tools/toollib.c
@@ -108,50 +108,50 @@ int become_daemon(struct cmd_context *cmd, int skip_lvm)
 const char *skip_dev_dir(struct cmd_context *cmd, const char *vg_name,
 			 unsigned *dev_dir_found)
 {
-	const char *dmdir = dm_dir();
+	size_t devdir_len = strlen(cmd->dev_dir);
+	const char *dmdir = dm_dir() + devdir_len;
 	size_t dmdir_len = strlen(dmdir), vglv_sz;
 	char *vgname, *lvname, *layer, *vglv;
 
 	/* FIXME Do this properly */
-	if (*vg_name == '/') {
-		while (*vg_name == '/')
+	if (*vg_name == '/')
+		while (vg_name[1] == '/')
 			vg_name++;
-		vg_name--;
-	}
 
-	/* Reformat string if /dev/mapper found */
-	if (!strncmp(vg_name, dmdir, dmdir_len) && vg_name[dmdir_len] == '/') {
+	if (strncmp(vg_name, cmd->dev_dir, devdir_len)) {
+		if (dev_dir_found)
+			*dev_dir_found = 0;
+	} else {
 		if (dev_dir_found)
 			*dev_dir_found = 1;
-		vg_name += dmdir_len;
+
+		vg_name += devdir_len;
 		while (*vg_name == '/')
 			vg_name++;
 
-		if (!dm_split_lvm_name(cmd->mem, vg_name, &vgname, &lvname, &layer) ||
-		    *layer) {
-			log_error("skip_dev_dir: Couldn't split up device name %s",
-				  vg_name);
-			return vg_name;
+		/* Reformat string if /dev/mapper found */
+		if (!strncmp(vg_name, dmdir, dmdir_len) && vg_name[dmdir_len + 1] == '/') {
+			vg_name += devdir_len + 1;
+			while (*vg_name == '/')
+				vg_name++;
+
+			if (!dm_split_lvm_name(cmd->mem, vg_name, &vgname, &lvname, &layer) ||
+			    *layer) {
+				log_error("skip_dev_dir: Couldn't split up device name %s",
+					  vg_name);
+				return vg_name;
+			}
+			vglv_sz = strlen(vgname) + strlen(lvname) + 2;
+			if (!(vglv = dm_pool_alloc(cmd->mem, vglv_sz)) ||
+			    dm_snprintf(vglv, vglv_sz, "%s%s%s", vgname,
+					*lvname ? "/" : "",
+					lvname) < 0) {
+				log_error("vg/lv string alloc failed");
+				return vg_name;
+			}
+			return vglv;
 		}
-		vglv_sz = strlen(vgname) + strlen(lvname) + 2;
-		if (!(vglv = dm_pool_alloc(cmd->mem, vglv_sz)) ||
-		    dm_snprintf(vglv, vglv_sz, "%s%s%s", vgname,
-				 *lvname ? "/" : "",
-				 lvname) < 0) {
-			log_error("vg/lv string alloc failed");
-			return vg_name;
-		}
-		return vglv;
 	}
-
-	if (!strncmp(vg_name, cmd->dev_dir, strlen(cmd->dev_dir))) {
-		if (dev_dir_found)
-			*dev_dir_found = 1;
-		vg_name += strlen(cmd->dev_dir);
-		while (*vg_name == '/')
-			vg_name++;
-	} else if (dev_dir_found)
-		*dev_dir_found = 0;
 
 	return vg_name;
 }

commit 4bfdb01f789a50267cbb50288a3bd5bc60edd685

toollib: fix regression in parsing /dev/mapper/vg-lv
Commit b0dde9e8f026ddd679 introduced regression in parsing
/dev/mapper prefix - and tried to check for '/' one char behind.

WHATS_NEW
@@ -1,5 +1,6 @@
 Version 2.02.114 - 
 =====================================
+  Fix regression when parsing /dev/mapper dir (2.02.112).
   Fix missing rounding to 64KB when estimating optimal thin pool chunk size.
   Fix typo in clvmd initscript causing CLVMD_STOP_TIMEOUT variable to be ignored.
   Fix size in pvresize "Resizing to ..." verbose msg to show proper result size.

tools/toollib.c
@@ -130,8 +130,8 @@ const char *skip_dev_dir(struct cmd_context *cmd, const char *vg_name,
 			vg_name++;
 
 		/* Reformat string if /dev/mapper found */
-		if (!strncmp(vg_name, dmdir, dmdir_len) && vg_name[dmdir_len + 1] == '/') {
-			vg_name += devdir_len + 1;
+		if (!strncmp(vg_name, dmdir, dmdir_len) && vg_name[dmdir_len] == '/') {
+			vg_name += dmdir_len + 1;
 			while (*vg_name == '/')
 				vg_name++;
 

test/shell/listings.sh
@@ -68,6 +68,11 @@ test $(lvs --noheadings $vg | wc -l) -eq 2
 test $(lvs -a --noheadings $vg | wc -l) -eq 6
 dmsetup ls | grep "$PREFIX" | grep -v "LVMTEST.*pv."
 
+# Check we parse /dev/mapper/vg-lv
+lvdisplay "$DM_DEV_DIR/mapper/$vg-$lv3"
+# Check we parse /dev/vg/lv
+lvdisplay "$DM_DEV_DIR/$vg/$lv3"
+
 lvcreate -l2 -s $vg/$lv3
 lvcreate -l1 -s -n inval $vg/$lv3
 lvcreate -l4 -I4 -i2 -n stripe $vg