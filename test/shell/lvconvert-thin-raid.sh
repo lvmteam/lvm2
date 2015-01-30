d021284bcfda0222f10787ea798077f312ee3275
Zdenek Kabelac <zkabelac@redhat.com>
2015-01-30 12:30:35 +0100

    tests: test stacked raid

--- test/shell/lvconvert-thin-raid.sh
+++ test/shell/lvconvert-thin-raid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2014 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2014-2015 Red Hat, Inc. All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
@@ -18,8 +18,10 @@ aux have_raid 1 4 0 || skip
 aux prepare_vg 4
 
 # create RAID LVs for data and metadata volumes
-lvcreate -aey --nosync -L10M --type raid1 -m1 -n $lv1 $vg
-lvcreate -aey --nosync -L8M --type raid1 -m1 -n $lv2 $vg
+lvcreate -aey -L10M --type raid1 -m3 -n $lv1 $vg
+lvcreate -aey -L8M --type raid1 -m3 -n $lv2 $vg
+aux wait_for_sync $vg $lv1
+aux wait_for_sync $vg $lv2
 lvchange -an $vg/$lv1
 
 # conversion fails for internal volumes
@@ -27,5 +29,23 @@ invalid lvconvert --thinpool $vg/${lv1}_rimage_0
 invalid lvconvert --yes --thinpool $vg/$lv1 --poolmetadata $vg/${lv2}_rimage_0
 
 lvconvert --yes --thinpool $vg/$lv1 --poolmetadata $vg/$lv2
+
+lvchange -ay $vg
+
+lvconvert --splitmirrors 1 --name data2 $vg/${lv1}_tdata "$dev2"
+lvconvert --splitmirrors 1 --name data3 $vg/${lv1}_tdata "$dev3"
+lvconvert --splitmirrors 1 --trackchanges $vg/${lv1}_tdata "$dev4"
+
+lvconvert --splitmirrors 1 --name meta1 $vg/${lv1}_tmeta "$dev1"
+lvconvert --splitmirrors 1 --name meta2 $vg/${lv1}_tmeta "$dev2"
+lvconvert --splitmirrors 1 --trackchanges $vg/${lv1}_tmeta "$dev4"
+
+lvremove -ff $vg/data2 $vg/data3 $vg/meta1 $vg/meta2
+
+lvconvert --merge $vg/${lv1}_tdata_rimage_1
+lvconvert --merge $vg/${lv1}_tmeta_rimage_1
+
+lvconvert -m+1 $vg/${lv1}_tdata "$dev2"
+lvconvert -m+1 $vg/${lv1}_tmeta "$dev1"
 
 vgremove -ff $vg
