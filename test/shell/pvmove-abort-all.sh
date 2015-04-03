ce3c61c4af4622674d8ebd4301d4fd24c4cb8886
Zdenek Kabelac <zkabelac@redhat.com>
2015-04-03 14:39:05 +0200

    tests: delay only data area

--- test/shell/pvmove-abort-all.sh
+++ test/shell/pvmove-abort-all.sh
@@ -34,18 +34,16 @@ lvcreate -an -Zn -l30 -n $lv1 $vg1 "$dev4"
 lvextend -l+30 -n $vg1/$lv1 "$dev5"
 
 # Slowdown writes
-aux delay_dev "$dev3" 0 300
-aux delay_dev "$dev6" 0 300
+aux delay_dev "$dev3" 100 100 $(get first_extent_sector "$dev3"):
+aux delay_dev "$dev6" 100 100 $(get first_extent_sector "$dev6"):
 
 pvmove -i1 $backgroundarg "$dev1" "$dev3" $mode &
 aux wait_pvmove_lv_ready "$vg-pvmove0" 300
 pvmove -i1 $backgroundarg "$dev2" "$dev3" $mode &
 aux wait_pvmove_lv_ready "$vg-pvmove1" 300
-aux delay_dev "$dev3" 0 1000
 
 pvmove -i1 $backgroundarg -n $vg1/$lv1 "$dev4" "$dev6" $mode &
 aux wait_pvmove_lv_ready "$vg1-pvmove0" 300
-aux delay_dev "$dev6" 0 1000
 
 # test removal of all pvmove LVs
 pvmove  --abort
@@ -60,12 +58,10 @@ not grep "^\[pvmove" out
 aux enable_dev "$dev3"
 aux enable_dev "$dev6"
 
-lvremove -ff $vg
-lvremove -ff $vg1
+lvremove -ff $vg $vg1
 
 wait
 done
 done
 
-vgremove -ff $vg
-vgremove -ff $vg1
+vgremove -ff $vg $vg1
