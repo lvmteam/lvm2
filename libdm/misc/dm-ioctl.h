19066191877029a1fe376a7d4a657e0dba1dd13d
Alasdair G Kergon <agk@redhat.com>
2015-04-23 18:39:04 +0100

    libdm: Add DM_INTERNAL_SUSPEND_FLAG.
    Still needs to be reported by dmsetup.

--- libdm/misc/dm-ioctl.h
+++ libdm/misc/dm-ioctl.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2001 - 2003 Sistina Software (UK) Limited.
- * Copyright (C) 2004 - 2014 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2004 - 2015 Red Hat, Inc. All rights reserved.
  *
  * This file is released under the LGPL.
  */
@@ -269,9 +269,9 @@ enum {
 #define DM_DEV_SET_GEOMETRY	_IOWR(DM_IOCTL, DM_DEV_SET_GEOMETRY_CMD, struct dm_ioctl)
 
 #define DM_VERSION_MAJOR	4
-#define DM_VERSION_MINOR	27
+#define DM_VERSION_MINOR	31
 #define DM_VERSION_PATCHLEVEL	0
-#define DM_VERSION_EXTRA	"-ioctl (2013-10-30)"
+#define DM_VERSION_EXTRA	"-ioctl (2015-03-12)"
 
 /* Status bits */
 #define DM_READONLY_FLAG	(1 << 0) /* In/Out */
@@ -353,5 +353,10 @@ enum {
  * gets closed.
  */
 #define DM_DEFERRED_REMOVE		(1 << 17) /* In/Out */
+
+/*
+ * If set, the device is suspended internally.
+ */
+#define DM_INTERNAL_SUSPEND_FLAG	(1 << 18) /* Out */
 
 #endif				/* _LINUX_DM_IOCTL_H */
