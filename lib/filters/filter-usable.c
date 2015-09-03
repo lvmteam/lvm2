lvmteam/lvm2

commit 2fc126b00d83991c6a2f3ed9aa61457294a4c45e

filter-usable: move check for pv_min_size from filter-partitioned to filter-usable and use new 'udev' external device info source for this check
The filter-partitioned code should contain only checks in "partition" domain.

The check for pv_min_size should actually be a part of filter-usable.
If the device size is less than pv_min_size, such device is not usable
as a PV so this check clearly belongs here logically.

With udev external info source, we can get device size via libudev's
sysfs reading interface and we can avoid opening the device this way
effectively.

lib/filters/filter-usable.c
@@ -15,6 +15,100 @@
 #include "lib.h"
 #include "filter.h"
 #include "activate.h" /* device_is_usable */
+#ifdef UDEV_SYNC_SUPPORT
+#include <libudev.h>
+#endif
+
+static const char *_too_small_to_hold_pv_msg = "Too small to hold a PV";
+
+static int _native_check_pv_min_size(struct device *dev)
+{
+	uint64_t size;
+	int ret = 0;
+
+	/* Check it's accessible */
+	if (!dev_open_readonly_quiet(dev)) {
+		log_debug_devs("%s: Skipping: open failed [%s:%p]",
+				dev_name(dev), dev_ext_name(dev), dev->ext.handle);
+		return 0;
+	}
+
+	/* Check it's not too small */
+	if (!dev_get_size(dev, &size)) {
+		log_debug_devs("%s: Skipping: dev_get_size failed [%s:%p]",
+				dev_name(dev), dev_ext_name(dev), dev->ext.handle);
+		goto out;
+	}
+
+	if (size < pv_min_size()) {
+		log_debug_devs("%s: Skipping: %s [%s:%p]", dev_name(dev),
+				_too_small_to_hold_pv_msg,
+				dev_ext_name(dev), dev->ext.handle);
+		goto out;
+	}
+
+	ret = 1;
+out:
+	if (!dev_close(dev))
+		stack;
+
+	return ret;
+}
+
+#ifdef UDEV_SYNC_SUPPORT
+static int _udev_check_pv_min_size(struct device *dev)
+{
+	struct dev_ext *ext;
+	const char *size_str;
+	char *endp;
+	uint64_t size;
+
+	if (!(ext = dev_ext_get(dev)))
+		return_0;
+
+	if (!(size_str = udev_device_get_sysattr_value((struct udev_device *)ext->handle, "size"))) {
+		log_debug_devs("%s: Skipping: failed to get size from sysfs [%s:%p]",
+				dev_name(dev), dev_ext_name(dev), dev->ext.handle);
+		return 0;
+	}
+
+	errno = 0;
+	size = strtoull(size_str, &endp, 10);
+	if (errno || !endp || *endp) {
+		log_debug_devs("%s: Skipping: failed to parse size from sysfs [%s:%p]",
+				dev_name(dev), dev_ext_name(dev), dev->ext.handle);
+		return 0;
+	}
+
+	if (size < pv_min_size()) {
+		log_debug_devs("%s: Skipping: %s [%s:%p]", dev_name(dev),
+				_too_small_to_hold_pv_msg,
+				dev_ext_name(dev), dev->ext.handle);
+		return 0;
+	}
+
+	return 1;
+}
+#else
+static int _udev_check_pv_min_size(struct device *dev)
+{
+	return 1;
+}
+#endif
+
+static int _check_pv_min_size(struct device *dev)
+{
+	if (dev->ext.src == DEV_EXT_NONE)
+		return _native_check_pv_min_size(dev);
+
+	if (dev->ext.src == DEV_EXT_UDEV)
+		return _udev_check_pv_min_size(dev);
+
+	log_error(INTERNAL_ERROR "Missing hook for PV min size check "
+		  "using external device info source %s", dev_ext_name(dev));
+
+	return 0;
+}
 
 static int _passes_usable_filter(struct dev_filter *f, struct device *dev)
 {
@@ -22,7 +116,20 @@ static int _passes_usable_filter(struct dev_filter *f, struct device *dev)
 	struct dev_usable_check_params ucp = {0};
 	int r;
 
-	/* filter only device-mapper devices */
+	/* check if the device is not too small to hold a PV */
+	switch (mode) {
+		case FILTER_MODE_NO_LVMETAD:
+			/* fall through */
+		case FILTER_MODE_PRE_LVMETAD:
+			if (!_check_pv_min_size(dev))
+				return 0;
+			break;
+		case FILTER_MODE_POST_LVMETAD:
+			/* nothing to do here */
+			break;
+	}
+
+	/* further checks are done on dm devices only */
 	if (!dm_is_dm_major(MAJOR(dev->dev)))
 		return 1;
 

lib/filters/filter-partitioned.c
@@ -19,39 +19,23 @@
 static int _passes_partitioned_filter(struct dev_filter *f, struct device *dev)
 {
 	struct dev_types *dt = (struct dev_types *) f->private;
-	const char *name = dev_name(dev);
 	int ret = 0;
-	uint64_t size;
 
-	/* Check it's accessible */
 	if (!dev_open_readonly_quiet(dev)) {
-		log_debug_devs("%s: Skipping: open failed", name);
+		log_debug_devs("%s: Skipping: open failed", dev_name(dev));
 		return 0;
-	}
-
-	/* Check it's not too small */
-	if (!dev_get_size(dev, &size)) {
-		log_debug_devs("%s: Skipping: dev_get_size failed", name);
-		goto out;
-	}
-
-	if (size < pv_min_size()) {
-		log_debug_devs("%s: Skipping: Too small to hold a PV", name);
-		goto out;
 	}
 
 	if (dev_is_partitioned(dt, dev)) {
 		log_debug_devs("%s: Skipping: Partition table signature found",
-			       name);
+			       dev_name(dev));
 		goto out;
 	}
 
 	ret = 1;
-
-      out:
+out:
 	if (!dev_close(dev))
 		stack;
-
 	return ret;
 }
 

commit 81a9da8f6190b567bb26f28ff02be0e6b10cad9a

filters: swap device_is_usable test
Fix regression introduced with commit:
2fc126b00d83991c6a2f3ed9aa61457294a4c45e

This commit has moved  pv_min_size() test in front
of device_is_usable(). However pv_min_size needs to open device,
so it may have actually get blocked.

So restore the original order and first validate
dm device to be usable for open.

It's worth to note that such check is not 'race-free',
but it usually eliminates 99.99% of problems ;).

WHATS_NEW
@@ -1,5 +1,7 @@
 Version 2.02.130 - 
 ===================================
+  Check if dm device is usable before checking its size (2.02.116).
+  Extend parsing of cache_check version in configure.
   Make lvpoll error messages visible in lvmpolld's stderr and in syslog.
   Add 'make install_full_man' to install all man pages regardless of config.
 

lib/filters/filter-usable.c
@@ -112,26 +112,11 @@ static int _passes_usable_filter(struct dev_filter *f, struct device *dev)
 {
 	filter_mode_t mode = *((filter_mode_t *) f->private);
 	struct dev_usable_check_params ucp = {0};
-	int r;
-
-	/* check if the device is not too small to hold a PV */
-	switch (mode) {
-		case FILTER_MODE_NO_LVMETAD:
-			/* fall through */
-		case FILTER_MODE_PRE_LVMETAD:
-			if (!_check_pv_min_size(dev))
-				return 0;
-			break;
-		case FILTER_MODE_POST_LVMETAD:
-			/* nothing to do here */
-			break;
-	}
+	int r = 1;
 
 	/* further checks are done on dm devices only */
-	if (!dm_is_dm_major(MAJOR(dev->dev)))
-		return 1;
-
-	switch (mode) {
+	if (dm_is_dm_major(MAJOR(dev->dev))) {
+		switch (mode) {
 		case FILTER_MODE_NO_LVMETAD:
 			ucp.check_empty = 1;
 			ucp.check_blocked = 1;
@@ -160,10 +145,25 @@ static int _passes_usable_filter(struct dev_filter *f, struct device *dev)
 			ucp.check_error_target = 0;
 			ucp.check_reserved = 0;
 			break;
+		}
+
+		if (!(r = device_is_usable(dev, ucp)))
+			log_debug_devs("%s: Skipping unusable device.", dev_name(dev));
 	}
 
-	if (!(r = device_is_usable(dev, ucp)))
-		log_debug_devs("%s: Skipping unusable device", dev_name(dev));
+	if (r) {
+		/* check if the device is not too small to hold a PV */
+		switch (mode) {
+		case FILTER_MODE_NO_LVMETAD:
+			/* fall through */
+		case FILTER_MODE_PRE_LVMETAD:
+			r = _check_pv_min_size(dev);
+			break;
+		case FILTER_MODE_POST_LVMETAD:
+			/* nothing to do here */
+			break;
+		}
+	}
 
 	return r;
 }