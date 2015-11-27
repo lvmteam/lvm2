2d630ea78aa430608f914e6da37fb98b051d86fa
Bryn M. Reeves <bmr@redhat.com>
2015-11-27 17:57:33 +0000

    tests: add template tests/shell/dmstats-create.sh
    Add a template for testing 'dmstats create' operations.

---
+++ test/shell/dmstats-create.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright (C) 2009-2011 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+. lib/inittest
+
+# Don't attempt to test stats with driver < 4.33.00
+aux driver_at_least 4 33 || skip
+
+# ensure we can create devices (uses dmsetup, etc)
+#aux prepare_devs 5
+
+# FIXME: some actual tests here.
