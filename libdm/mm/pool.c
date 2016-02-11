lvmteam/lvm2

commit 197b5e6dc7dd8ec161ebe43c97fd2ac8384b3433

Word alignment for strings
Align strdup char* allocation just on 2 bytes.
It looks like wasting space to align strings on 8 bytes.
(Could be even 1byte - but for hashing it might eventually get better
perfomance - but probably hardly measurable).

TODO: check on various architectures it's not making any problems.

WHATS_NEW_DM
@@ -1,5 +1,6 @@
 Version 1.02.64 - 
 ===================================
+  Use word alignment for dm_pool_strdup() and dm_pool_strndup().
   Use dm_snprintf() to fix signess warning in dm_set_dev_dir().
   Use unsigned loop counter to fix signess warning in _other_node_ops().
   Fix const cast in dmsetup calls of dm_report_field_string().

libdm/mm/pool.c
@@ -27,7 +27,7 @@ void dm_pools_check_leaks(void);
 
 char *dm_pool_strdup(struct dm_pool *p, const char *str)
 {
-	char *ret = dm_pool_alloc(p, strlen(str) + 1);
+	char *ret = dm_pool_alloc_aligned(p, strlen(str) + 1, 2);
 
 	if (ret)
 		strcpy(ret, str);
@@ -37,7 +37,7 @@ char *dm_pool_strdup(struct dm_pool *p, const char *str)
 
 char *dm_pool_strndup(struct dm_pool *p, const char *str, size_t n)
 {
-	char *ret = dm_pool_alloc(p, n + 1);
+	char *ret = dm_pool_alloc_aligned(p, n + 1, 2);
 
 	if (ret) {
 		strncpy(ret, str, n);

libdm/mm/pool-debug.c
@@ -136,7 +136,7 @@ static struct block *_new_block(size_t s, unsigned alignment)
 	 * I don't think LVM will use anything but default
 	 * align.
 	 */
-	assert(alignment == DEFAULT_ALIGNMENT);
+	assert(alignment <= DEFAULT_ALIGNMENT);
 
 	if (!b) {
 		log_error("Out of memory");

commit 0baf66a992fbac92fa2c30e9bb8e74a5535ff45a

dm: alloc always 8byte aligned
Fixing regression caused by 197b5e6dc7dd8ec161ebe43c97fd2ac8384b3433.
So the 'TODO' part now finally know the answer - there is 'sparc64'
architecture which imposes limitation to read 64b words only through
64b aligned address.

Since we never could know how is the user going to use the returned
pointer and the userusually expects it's aligned on the highest CPU
required alignement, preserve it also for char*.

Fixes: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=809685
Reported-by: Anatoly Pugachev <matorola@gmail.com>

WHATS_NEW_DM
@@ -1,5 +1,6 @@
 Version 1.02.116 - 
 ====================================
+  Use fully aligned allocations for dm_pool_strdup/strndup() (1.02.64).
   Fix thin-pool table parameter feature order to match kernel output.
 
 Version 1.02.115 - 25th January 2016

libdm/mm/pool.c
@@ -48,17 +48,18 @@ static size_t pagesize_mask = 0;
 
 char *dm_pool_strdup(struct dm_pool *p, const char *str)
 {
-	char *ret = dm_pool_alloc_aligned(p, strlen(str) + 1, 2);
+	size_t len = strlen(str) + 1;
+	char *ret = dm_pool_alloc(p, len);
 
 	if (ret)
-		strcpy(ret, str);
+		memcpy(ret, str, len);
 
 	return ret;
 }
 
 char *dm_pool_strndup(struct dm_pool *p, const char *str, size_t n)
 {
-	char *ret = dm_pool_alloc_aligned(p, n + 1, 2);
+	char *ret = dm_pool_alloc(p, n + 1);
 
 	if (ret) {
 		strncpy(ret, str, n);