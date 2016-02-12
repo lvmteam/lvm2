672deaebc55464015065c2101a3de03e02190e01
Peter Rajnoha <prajnoha@redhat.com>
2016-02-12 16:29:39 +0100

    man: pvresize: remove old comment about inability to resize PV with more mdas

---
+++ test/shell/pvresize-mdas.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Copyright (C) 2016 Red Hat, Inc. All rights reserved.
+#
+# This copyrighted material is made available to anyone wishing to use,
+# modify, copy, or redistribute it subject to the terms and conditions
+# of the GNU General Public License v.2.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+SKIP_WITH_LVMPOLLD=1
+
+. lib/inittest
+
+aux prepare_devs 1 8
+
+pvcreate --setphysicalvolumesize 8m --metadatacopies 2 "$dev1"
+check pv_field "$dev1" pv_size 8.00m
+check pv_field "$dev1" pv_mda_count 2
+pvs "$dev1"
+
+pvresize --setphysicalvolumesize 4m "$dev1"
+check pv_field "$dev1" pv_size 4.00m
+check pv_field "$dev1" pv_mda_count 2
+pvs "$dev1"
