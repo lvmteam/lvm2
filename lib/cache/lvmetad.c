30a73d1604aeb318709a01f4cc238e2d872be8de
Zdenek Kabelac <zkabelac@redhat.com>
2016-02-22 10:45:01 +0100

    lvmetad: fix error path
    Coverity noticed the internal error path would  be using uninit struct.
    So always make sure reply is initilized.

--- lib/cache/lvmetad.c
+++ lib/cache/lvmetad.c
@@ -507,7 +507,7 @@ struct volume_group *lvmetad_vg_lookup(struct cmd_context *cmd, const char *vgna
 {
 	struct volume_group *vg = NULL;
 	struct volume_group *vg2 = NULL;
-	daemon_reply reply;
+	daemon_reply reply = { 0 };
 	int found;
 	char uuid[64];
 	struct format_instance *fid = NULL;
