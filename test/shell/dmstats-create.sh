183bd8ca0342f6f72630afcb42b453b88ee90ac6
Zdenek Kabelac <zkabelac@redhat.com>
2016-02-26 08:45:06 +0100

    tests: skip on older version
    Skip with older driver version.
    Update address.

--- test/shell/dmstats-create.sh
+++ test/shell/dmstats-create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2009-2011 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2016 Red Hat, Inc. All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
@@ -7,7 +7,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 . lib/inittest
 
@@ -24,4 +24,3 @@ dmstats create --start 0 --len 1 "$dev1"
 dmstats create --segments "$dev1"
 dmstats create --precise "$dev1"
 dmstats create --bounds 10ms,20ms,30ms "$dev1"
-
--- test/shell/dmstats-report.sh
+++ test/shell/dmstats-report.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright (C) 2009-2011 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2016 Red Hat, Inc. All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
@@ -7,9 +7,12 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 . lib/inittest
+
+# Don't attempt to test stats with driver < 4.33.00
+aux driver_at_least 4 33 || skip
 
 # ensure we can create devices (uses dmsetup, etc)
 aux prepare_devs 1
@@ -21,4 +24,3 @@ dmstats create --bounds 10ms,20ms,30ms "$dev1"
 dmstats report
 dmstats report --count 1
 dmstats report --histogram
-
