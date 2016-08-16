73df2aedf9788dcf2dbf09f20f8783c6d2108e75
Heinz Mauelshagen <heinzm@redhat.com>
2016-08-16 13:39:40 +0200

    lvcreate: better error message creating RAID LV on < 4 KiB VG extent size
    Creating a RaidLV in VGs with very small extent sizes caused
    late failure in the kernel giving a not very informative error
    message. Catch the attempt early and display failure message
    'Unable to create RAID LV: requires minimum VG extent size 4.00 KiB'.
    
    - resoves rhbz1179970

--- tools/lvcreate.c
+++ tools/lvcreate.c
@@ -1255,9 +1255,16 @@ static int _check_raid_parameters(struct volume_group *vg,
 				  struct lvcreate_cmdline_params *lcp)
 {
 	unsigned devs = lcp->pv_count ? : dm_list_size(&vg->pvs);
+	uint64_t page_sectors = lvm_getpagesize() >> SECTOR_SHIFT;
 	struct cmd_context *cmd = vg->cmd;
 	int old_stripes = !arg_is_set(cmd, stripes_ARG) &&
 			  find_config_tree_bool(cmd, allocation_raid_stripe_all_devices_CFG, NULL);
+
+	if (vg->extent_size < page_sectors) {
+		log_error("Unable to create RAID LV: requires minimum VG extent size %s",
+			  display_size(vg->cmd, page_sectors));
+		return 0;
+	}
 
 	/*
 	 * If we requested the previous behaviour by setting
