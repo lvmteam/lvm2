4a33e4c509fb72bf3540d20bd35e1f0944e1d37a
Zdenek Kabelac <zkabelac@redhat.com>
2016-12-09 23:10:00 +0100

    cov: declaration matching

--- lib/format_text/format-text.h
+++ lib/format_text/format-text.h
@@ -59,7 +59,7 @@ int add_da(struct dm_pool *mem, struct dm_list *das,
 void del_das(struct dm_list *das);
 int add_ba(struct dm_pool *mem, struct dm_list *eas,
 	   uint64_t start, uint64_t size);
-void del_bas(struct dm_list *eas);
+void del_bas(struct dm_list *bas);
 int add_mda(const struct format_type *fmt, struct dm_pool *mem, struct dm_list *mdas,
 	    struct device *dev, uint64_t start, uint64_t size, unsigned ignored);
 void del_mdas(struct dm_list *mdas);
