1cdaa8144ddc2d171700d2c80f18cb745828f7ed
Alasdair G Kergon <agk@redhat.com>
2017-03-16 01:36:02 +0000

    lvmsar/lvmsadc: Mention dmstats when run.

--- tools/stub.h
+++ tools/stub.h
@@ -13,22 +13,29 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+/*
 #define unimplemented \
 	log_error("Command not implemented yet."); return ECMD_FAILED
 
-/*int e2fsadm(struct cmd_context *cmd, int argc, char **argv) unimplemented*/
+int e2fsadm(struct cmd_context *cmd, int argc, char **argv) unimplemented
+*/
+
 int lvmsadc(struct cmd_context *cmd __attribute__((unused)),
 	    int argc __attribute__((unused)),
 	    char **argv __attribute__((unused)))
 {
-	unimplemented;
+	log_error("There's no 'lvmsadc' command in LVM2.");
+	log_error("Please use the superior 'dmstats' facilities instead.");
+	return ECMD_FAILED;
 }
 
 int lvmsar(struct cmd_context *cmd __attribute__((unused)),
 	   int argc __attribute__((unused)),
 	   char **argv __attribute__((unused)))
 {
-	unimplemented;
+	log_error("There's no 'lvmsar' command in LVM2.");
+	log_error("Please use the superior 'dmstats' facilities instead.");
+	return ECMD_FAILED;
 }
 
 int pvdata(struct cmd_context *cmd __attribute__((unused)),
