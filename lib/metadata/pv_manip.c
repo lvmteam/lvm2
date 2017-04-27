0e3c16af5699c000f1824c42c596fec58d019dfa
Alasdair G Kergon <agk@redhat.com>
2017-04-27 14:59:40 +0100

    pvresize: Missing a message on error path.

--- lib/metadata/pv_manip.c
+++ lib/metadata/pv_manip.c
@@ -648,14 +648,18 @@ int pv_resize_single(struct cmd_context *cmd,
 				 pv_name, display_size(cmd, (uint64_t) size));
 			if (!yes && yes_no_prompt("%s: Requested size %s exceeds real size %s. Proceed?  [y/n]: ",
 						  pv_name, display_size(cmd, (uint64_t) new_size),
-						  display_size(cmd, (uint64_t) size)) == 'n')
+						  display_size(cmd, (uint64_t) size)) == 'n') {
+				log_error("Physical Volume %s not resized.", pv_name);
 				goto_out;
+			}
 
 		}  else if (new_size < size)
 			if (!yes && yes_no_prompt("%s: Requested size %s is less than real size %s. Proceed?  [y/n]: ",
 						  pv_name, display_size(cmd, (uint64_t) new_size),
-						  display_size(cmd, (uint64_t) size)) == 'n')
+						  display_size(cmd, (uint64_t) size)) == 'n') {
+				log_error("Physical Volume %s not resized.", pv_name);
 				goto_out;
+			}
 
 		if (new_size == size)
 			log_verbose("%s: Size is already %s (%" PRIu64 " sectors).",
