lvmteam/lvm2

commit 70bb72667889a2d6f250437e5db247eb111ca1aa

dmfilemapd: always close check file descriptor (coverity)
The daemon opens a new file descriptor from fm->path when checking
for an unlinked file with mode=inode. Ensure that it is always
closed even if the same file test fails.

daemons/dmfilemapd/dmfilemapd.c
@@ -566,11 +566,13 @@ static int _filemap_monitor_check_file_unlinked(struct filemap_monitor *fm)
 	if ((fd = open(fm->path, O_RDONLY)) < 0)
 		goto check_unlinked;
 
-	if ((same = _filemap_monitor_check_same_file(fm->fd, fd)) < 0)
-		return 0;
+	same = _filemap_monitor_check_same_file(fm->fd, fd);
 
 	if (close(fd))
 		log_error("Error closing fd %d", fd);
+
+	if (same < 0)
+		return 0;
 
 	if (same)
 		return 1;

commit a9940d16fe5251f2f1d7cb008cfd3be1bfb50120

dmfilemapd: always initialise 'same' local variable (Coverity)
Fix a regression introduced in 70bb726 that allows a local variable
in the monitored file checking routine to be accessed before its
assignment when the file has already been unlinked.

daemons/dmfilemapd/dmfilemapd.c
@@ -562,6 +562,7 @@ static int _filemap_monitor_check_file_unlinked(struct filemap_monitor *fm)
 	ssize_t len;
 
 	fm->deleted = 0;
+	same = 0;
 
 	if ((fd = open(fm->path, O_RDONLY)) < 0)
 		goto check_unlinked;