3797f47ecf23c41d4476e2cce0f210b48b32923d
David Teigland <teigland@redhat.com>
2017-07-07 14:42:25 -0500

    lvmlockd: fix revert in lvcreate
    If the activation step in lvcreate fails (e.g. the specified
    minor number is already used), then the lvcreate is reverted,
    but the LV lock in lvmlockd was not being unlocked or properly
    freed.

--- lib/metadata/lv_manip.c
+++ lib/metadata/lv_manip.c
@@ -7983,7 +7983,8 @@ deactivate_and_revert_new_lv:
 	}
 
 revert_new_lv:
-	lockd_free_lv(vg->cmd, vg, lp->lv_name, &lv->lvid.id[1], lp->lock_args);
+	lockd_lv(cmd, lv, "un", LDLV_PERSISTENT);
+	lockd_free_lv(vg->cmd, vg, lp->lv_name, &lv->lvid.id[1], lv->lock_args);
 
 	/* FIXME Better to revert to backup of metadata? */
 	if (!lv_remove(lv) || !vg_write(vg) || !vg_commit(vg))
