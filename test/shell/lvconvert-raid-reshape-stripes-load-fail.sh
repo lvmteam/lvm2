86fa0333fffe89547263f8b46b291d19726e7cca
Heinz Mauelshagen <heinzm@redhat.com>
2017-12-13 02:55:06 +0100

    test: new raid tests
    Add three new raid tests with io load and table
    reloads during reshape for target 1.13.2.
    
    Add a raid0 to raid10 conversion test.
    
    Also add more signals to trap in lvconvert-raid-reshape-load.sh.

--- test/shell/lvconvert-raid-reshape-stripes-load-fail.sh
+++ test/shell/lvconvert-raid-reshape-stripes-load-fail.sh
@@ -33,29 +33,37 @@ get_devs
 vgcreate -s 1M "$vg" "${DEVICES[@]}"
 
 # Create 13-way striped raid5 (14 legs total)
-lvcreate --yes --type raid5_ls --stripes 13 -L190M -n$lv1 $vg
+lvcreate --yes --type raid5_ls --stripesize 64K --stripes 10 -L200M -n$lv1 $vg
 check lv_first_seg_field $vg/$lv1 segtype "raid5_ls"
-check lv_first_seg_field $vg/$lv1 data_stripes 13
-check lv_first_seg_field $vg/$lv1 stripes 14
+check lv_first_seg_field $vg/$lv1 stripesize "64.00k"
+check lv_first_seg_field $vg/$lv1 data_stripes 10
+check lv_first_seg_field $vg/$lv1 stripes 11
 echo y|mkfs -t ext4 /dev/$vg/$lv1
-aux wait_for_sync $vg $lv1
 
 mkdir -p $mpoint
 mount "$DM_DEV_DIR/$vg/$lv1" $mpoint
 mkdir -p $mpoint/1 $mpoint/2
 
-aux delay_dev "$dev2" 0 100
 
 echo 3 >/proc/sys/vm/drop_caches
 cp -r /usr/bin $mpoint/1 >/dev/null 2>/dev/null &
 cp -r /usr/bin $mpoint/2 >/dev/null 2>/dev/null &
 sync &
 
-# Reshape it to 256K stripe size
-lvconvert --yes --stripesize 256 $vg/$lv1
-aux delay_dev "$dev2" 0 0
-check lv_first_seg_field $vg/$lv1 stripesize "256.00k"
+aux wait_for_sync $vg $lv1
+aux delay_dev "$dev2" 0 100
 
+# Reshape it to 15 data stripes
+lvconvert --yes --stripes 15 $vg/$lv1
+aux disable_dev $dev1
+aux delay_dev "$dev2" 0 0
+check lv_first_seg_field $vg/$lv1 segtype "raid5_ls"
+check lv_first_seg_field $vg/$lv1 stripesize "64.00k"
+check lv_first_seg_field $vg/$lv1 data_stripes 15
+check lv_first_seg_field $vg/$lv1 stripes 16
+
+rm -fr $mpoint/2
+sync
 kill -9 %%
 wait
 
--- test/shell/lvconvert-raid-reshape-stripes-load-reload.sh
+++ test/shell/lvconvert-raid-reshape-stripes-load-reload.sh
@@ -20,7 +20,7 @@ LVM_SKIP_LARGE_TESTS=1
 # Test reshaping under io load
 
 which mkfs.ext4 || skip
-aux have_raid 1 13 1 || skip
+aux have_raid 1 13 2 || skip
 
 mpoint=/tmp/mpoint.$$
 
@@ -33,28 +33,42 @@ get_devs
 vgcreate -s 1M "$vg" "${DEVICES[@]}"
 
 # Create 13-way striped raid5 (14 legs total)
-lvcreate --yes --type raid5_ls --stripes 13 -L190M -n$lv1 $vg
+lvcreate --yes --type raid5_ls --stripesize 64K --stripes 10 -L200M -n$lv1 $vg
 check lv_first_seg_field $vg/$lv1 segtype "raid5_ls"
-check lv_first_seg_field $vg/$lv1 data_stripes 13
-check lv_first_seg_field $vg/$lv1 stripes 14
+check lv_first_seg_field $vg/$lv1 stripesize "64.00k"
+check lv_first_seg_field $vg/$lv1 data_stripes 10
+check lv_first_seg_field $vg/$lv1 stripes 11
 echo y|mkfs -t ext4 /dev/$vg/$lv1
-aux wait_for_sync $vg $lv1
 
 mkdir -p $mpoint
 mount "$DM_DEV_DIR/$vg/$lv1" $mpoint
 mkdir -p $mpoint/1 $mpoint/2
-
-aux delay_dev "$dev2" 0 100
 
 echo 3 >/proc/sys/vm/drop_caches
 cp -r /usr/bin $mpoint/1 >/dev/null 2>/dev/null &
 cp -r /usr/bin $mpoint/2 >/dev/null 2>/dev/null &
 sync &
 
-# Reshape it to 256K stripe size
-lvconvert --yes --stripesize 256 $vg/$lv1
-aux delay_dev "$dev2" 0 0
-check lv_first_seg_field $vg/$lv1 stripesize "256.00k"
+aux wait_for_sync $vg $lv1
+aux delay_dev "$dev2" 0 200
+
+# Reshape it to 15 data stripes
+lvconvert --yes --stripes 15 $vg/$lv1
+check lv_first_seg_field $vg/$lv1 segtype "raid5_ls"
+check lv_first_seg_field $vg/$lv1 stripesize "64.00k"
+check lv_first_seg_field $vg/$lv1 data_stripes 15
+check lv_first_seg_field $vg/$lv1 stripes 16
+
+# Reload table during reshape to test for data corruption
+for i in {0..5}
+do
+	dmsetup table $vg-$lv1|dmsetup load $vg-$lv1
+	dmsetup suspend --noflush $vg-$lv1
+	dmsetup resume $vg-$lv1
+	sleep 0.3
+done
+
+aux delay_dev "$dev2" 0
 
 kill -9 %%
 wait
