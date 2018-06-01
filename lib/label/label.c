7b5b1a9b6f03e9452c7a9d602247e61e808c316b
David Teigland <teigland@redhat.com>
2018-06-01 10:40:28 -0500

    scan: clean exit for alloc failure

--- lib/label/label.c
+++ lib/label/label.c
@@ -832,7 +832,7 @@ int label_scan(struct cmd_context *cmd)
 
 	while ((dev = dev_iter_get(iter))) {
 		if (!(devl = dm_zalloc(sizeof(*devl))))
-			return 0;
+			continue;
 		devl->dev = dev;
 		dm_list_add(&all_devs, &devl->list);
 
