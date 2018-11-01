d037430be05a610980533bff6eda55181cbbed97
Bryn M. Reeves <bmr@redhat.com>
2018-11-01 16:49:05 +0000

    dmsetup: fix stats report command output
    Since the stats handle is neither bound nor listed before the
    attempt to call dm_stats_get_nr_regions(), it will always return
    zero: this prevents reporting of any dmstats regions on any
    device.
    
    Remove the dm_stats_get_nr_regions() check and instead rely on
    the correct return status from dm_stats_populate() which only
    returns 0 in the case that there are regions to inspect (and
    which logs a specific error for all other cases).
    
    Reported-by: Bryan Gurney <bgurney@redhat.com>

--- libdm/dm-tools/dmsetup.c
+++ libdm/dm-tools/dmsetup.c
@@ -914,16 +914,12 @@ static int _display_info_cols(struct dm_task *dmt, struct dm_info *info)
 		if (!(obj.stats = dm_stats_create(DM_STATS_PROGRAM_ID)))
 			goto_out;
 
-		if (!dm_stats_get_nr_regions(obj.stats)) {
-			log_debug("Skipping %s with no regions.", dm_task_get_name(dmt));
+		dm_stats_bind_devno(obj.stats, info->major, info->minor);
+
+		if (!dm_stats_populate(obj.stats, _program_id, DM_STATS_REGIONS_ALL)) {
 			r = 1;
 			goto out;
 		}
-
-		dm_stats_bind_devno(obj.stats, info->major, info->minor);
-
-		if (!dm_stats_populate(obj.stats, _program_id, DM_STATS_REGIONS_ALL))
-			goto_out;
 
 		/* Update timestamps and handle end-of-interval accounting. */
 		_update_interval_times();
