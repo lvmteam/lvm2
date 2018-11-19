cb15373ad705a360c4a32d4ca9e97af949fec17a
Zdenek Kabelac <zkabelac@redhat.com>
2018-11-19 17:16:36 +0100

    tests: missing copyright

--- test/lib/dmsecuretest.c
+++ test/lib/dmsecuretest.c
@@ -1,6 +1,18 @@
 /*
+ * Copyright (C) 2018 Red Hat, Inc. All rights reserved.
+ *
+ * This file is part of LVM2.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License v.2.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
  * Test sample code to check for leftovers from secure table loading in
- * userspace memory
+ * userspace memory (initial sample provided by Milan Broz).
  *
  * Compile with:  gcc -O2 -g -o tst dmcrypt.c -ldevmapper
  *
