lvmteam/lvm2

commit 668c9d0762fc6736a1e9372785544eda6a939806

cov: check dm_strncpy result

device_mapper/vdo/status.c
@@ -207,7 +207,11 @@ bool dm_vdo_status_parse(struct dm_pool *mem, const char *input,
 		_set_error(result, "out of memory");
 		goto bad;
 	}
-	dm_strncpy(s->device, b, te - b + 1);
+
+	if (!dm_strncpy(s->device, b, te - b + 1)) {
+		_set_error(result, "copy device");
+		goto bad;
+	}
 
 	b = _eat_space(te, e);
 

commit c61c4271a4d9cc990f19614d1d2e7d203d57ebfd

device_mapper: fix incorrect dm_strncpy usage
Patch 668c9d0762fc6736a1e9372785544eda6a939806 introduced regression,
since the code here would actually always return failing result.
Replace it with more simple call to strndup().

device_mapper/vdo/status.c
@@ -203,13 +203,8 @@ bool dm_vdo_status_parse(struct dm_pool *mem, const char *input,
 		goto bad;
 	}
 
-	if (!(s->device = (!mem) ? malloc((e - b) + 1) : dm_pool_alloc(mem, (e - b) + 1))) {
+	if (!(s->device = (!mem) ? strndup(b, (te - b)) : dm_pool_alloc(mem, (te - b)))) {
 		_set_error(result, "out of memory");
-		goto bad;
-	}
-
-	if (!dm_strncpy(s->device, b, te - b + 1)) {
-		_set_error(result, "copy device");
 		goto bad;
 	}
 