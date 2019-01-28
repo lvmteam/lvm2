cbf62b9262d493a5df9ab5a57e68395465d4701a
David Teigland <teigland@redhat.com>
2019-01-28 11:38:05 -0600

    tests: use pvscan after enable_dev in process-each-duplicate-vgnames
    instead of vgscan, so that new dev is recognized with hints

--- test/shell/process-each-duplicate-vgnames.sh
+++ test/shell/process-each-duplicate-vgnames.sh
@@ -20,20 +20,16 @@ pvcreate "$dev2"
 aux disable_dev "$dev1" "$dev2"
 
 aux enable_dev "$dev1"
-vgscan
 vgcreate $vg1 "$dev1"
 UUID1=$(vgs --noheading -o vg_uuid $vg1)
 aux disable_dev "$dev1"
 
 aux enable_dev "$dev2"
-vgscan
 vgcreate $vg1 "$dev2"
 UUID2=$(vgs --noheading -o vg_uuid $vg1)
 
 aux enable_dev "$dev1"
-# need vgscan after enabling/disabling devs
-# so that the next commands properly see them
-vgscan
+pvscan --cache "$dev1"
 pvs "$dev1"
 pvs "$dev2"
 
@@ -50,11 +46,10 @@ vgs -o+vg_uuid | tee err
 grep $UUID1 err
 not grep $UUID2 err
 aux enable_dev "$dev2"
-vgscan
+pvscan --cache "$dev2"
 
 aux disable_dev "$dev1"
 vgs -o+vg_uuid | tee err
 grep $UUID2 err
 not grep $UUID1 err
 aux enable_dev "$dev1"
-vgscan
