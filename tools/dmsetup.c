53fcd4fd8fdca852bd8db079f943594c4deeded5
Zdenek Kabelac <zkabelac@redhat.com>
2019-08-16 23:49:38 +0200

    dmsetup: debug print
    Udev debugging is a bit tricky, so to more easily pair cookie ID,
    which is the lowest 16 bit - print cookie as hexa number.
    This simplify pairing of processed cookies while the 'higher bit flags'
    are changed for the same cookie.

--- tools/dmsetup.c
+++ tools/dmsetup.c
@@ -1694,6 +1694,7 @@ static int _udevcomplete(CMD_ARGS)
 	if (!(cookie = _get_cookie_value(argv[0])))
 		return_0;
 
+	printf("DM_COOKIE_COMPLETED=0x%-10x", cookie);
 	/*
 	 * Strip flags from the cookie and use cookie magic instead.
 	 * If the cookie has non-zero prefix and the base is zero then
