a087da2f51e2b455a056bcf0550cb02a65d40a53
Heinz Mauelshagen <heinzm@redhat.com>
2019-10-02 14:48:53 +0200

    test: add checks for not 100% sync ratio after initiation of check/repair
    Related: rhbz1640630

--- test/shell/lvchange-raid.sh
+++ test/shell/lvchange-raid.sh
@@ -139,6 +139,7 @@ run_writemostly_check() {
 	check lv_attr_bit health $vg/${lv}_rimage_1 "-"
 }
 
+
 # run_syncaction_check <VG> <LV>
 run_syncaction_check() {
 	local device
@@ -183,17 +184,20 @@ run_syncaction_check() {
 	# "check" should find discrepancies but not change them
 	# 'lvs' should show results
 	lvchange --syncaction check $vg/$lv
+	not lv_field $vg/$lv sync_percent "100.00"
 	aux wait_for_sync $vg $lv
 	check lv_attr_bit health $vg/$lv "m"
 	not check lv_field $vg/$lv raid_mismatch_count "0"
 
 	# "repair" will fix discrepancies
 	lvchange --syncaction repair $vg/$lv
+	not lv_field $vg/$lv sync_percent "100.00"
 	aux wait_for_sync $vg $lv
 
 	# Final "check" should show no mismatches
 	# 'lvs' should show results
 	lvchange --syncaction check $vg/$lv
+	not lv_field $vg/$lv sync_percent "100.00"
 	aux wait_for_sync $vg $lv
 	check lv_attr_bit health $vg/$lv "-"
 	check lv_field $vg/$lv raid_mismatch_count "0"
