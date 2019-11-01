43db8f8d5d7c773d4fe93f3bbf7ee03be4f6e36e
Zdenek Kabelac <zkabelac@redhat.com>
2019-11-01 21:01:51 +0100

    cov: ensure read_ahead is available
    Make sure read_ahead pointer is not NULL when quering for RA.

--- lib/activate/dev_manager.c
+++ lib/activate/dev_manager.c
@@ -278,7 +278,7 @@ static int _info_run(const char *dlid, struct dm_info *dminfo,
 	    (strcmp(name_check, devname) != 0))
 		dminfo->exists = 0;	/* mismatching name -> device does not exist */
 
-	if (with_read_ahead && dminfo->exists) {
+	if (with_read_ahead && read_ahead && dminfo->exists) {
 		if (!dm_task_get_read_ahead(dmt, read_ahead))
 			goto_out;
 	} else if (read_ahead)
