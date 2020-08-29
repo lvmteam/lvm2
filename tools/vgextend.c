4baedfc5787e5b87cfb3f4b0ea13a6ae9bc9c52e
Zdenek Kabelac <zkabelac@redhat.com>
2020-08-29 21:53:31 +0200

    cleanup: add spaces between literals

--- tools/vgextend.c
+++ tools/vgextend.c
@@ -111,7 +111,7 @@ static int _vgextend_single(struct cmd_context *cmd, const char *vg_name,
 
 		if ((mda_copies != VGMETADATACOPIES_UNMANAGED) &&
 		    (mda_copies != mda_used)) {
-			log_warn("WARNING: Changing preferred number of copies of VG %s metadata from %"PRIu32" to %"PRIu32,
+			log_warn("WARNING: Changing preferred number of copies of VG %s metadata from %" PRIu32 " to %" PRIu32,
 				 vg_name, mda_copies, mda_used);
 			vg_set_mda_copies(vg, mda_used);
 		}
