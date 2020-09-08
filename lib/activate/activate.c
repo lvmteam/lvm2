ce5ea07411d57a2bd276d8d29e46ce30b957f657
Zdenek Kabelac <zkabelac@redhat.com>
2020-09-08 21:09:06 +0200

    raid: do not enforce flushing of raids when it is not required
    This is probably somewhat experimantal patch - but when i.e. raid device
    is just extend, there should not be a technical need for flush,
    unless the target would stricly need it.  It should allow faster
    processing of lvm command not being blocked by possibly longer flush.

--- lib/activate/activate.c
+++ lib/activate/activate.c
@@ -2177,7 +2177,7 @@ static int _lv_suspend(struct cmd_context *cmd, const char *lvid_s,
 	 * TODO: Relax this limiting condition further */
 	if (!flush_required &&
 	    (lv_is_pvmove(lv) || pvmove_lv ||
-	     (!lv_is_mirror(lv) && !lv_is_thin_pool(lv) && !lv_is_thin_volume(lv)))) {
+	     (!lv_is_raid(lv) && !lv_is_mirror(lv) && !lv_is_thin_pool(lv) && !lv_is_thin_volume(lv)))) {
 		log_debug("Requiring flush for LV %s.", display_lvname(lv));
 		flush_required = 1;
 	}
