3dcd61d3d797e3ed12e934f1fa7064153e2fdb71
Zdenek Kabelac <zkabelac@redhat.com>
2020-09-10 16:39:07 +0200

    tests: disable dbustest with valgrind testing

--- test/api/dbustest.sh
+++ test/api/dbustest.sh
@@ -16,6 +16,9 @@ SKIP_WITH_CLVMD=1
 
 . lib/inittest
 
+# Unsupported with valgrid testing
+test ${LVM_VALGRIND:-0} -eq 0 || skip "Unsupported with valgrind"
+
 # NOTE: Some tests, namely anything with vdo, and
 # api/dbus_test_lv_interface_cache_lv.sh, require larger PVs
 aux prepare_pvs 6 6400
