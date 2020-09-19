688c72411e206008d5cd7553ed0b126549b7f987
Zdenek Kabelac <zkabelac@redhat.com>
2020-09-19 15:58:11 +0200

    tests: use zero backend
    Since we are not reading read - just use zero device as backend for
    test, so we do not eat real disk space.

--- test/shell/lvcreate-large-raid.sh
+++ test/shell/lvcreate-large-raid.sh
@@ -38,7 +38,7 @@ aux lvmconf 'devices/issue_discards = 1'
 # Delay PVs so that resynchronization doesn't fill too much space
 for device in "${DEVICES[@]}"
 do
-	aux delay_dev "$device" 0 10  "$(get first_extent_sector "$device")"
+	aux zero_dev "$device" "$(( $(get first_extent_sector "$device") + 8192 )):"
 done
 
 # bz837927 START
