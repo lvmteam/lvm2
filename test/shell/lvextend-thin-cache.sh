b86eb913c18ca67415521b8003c04adf41a5496c
Zdenek Kabelac <zkabelac@redhat.com>
2021-02-07 18:45:43 +0100

    tests: support for resized cached LVs
    Support for cached LVs is now enabled.

--- test/shell/lvextend-thin-cache.sh
+++ test/shell/lvextend-thin-cache.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2017 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2017-2020 Red Hat, Inc. All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
@@ -22,15 +22,22 @@ export LVM_TEST_THIN_REPAIR_CMD=${LVM_TEST_THIN_REPAIR_CMD-/bin/false}
 aux have_thin 1 0 0 || skip
 aux have_cache 1 3 0 || skip
 
-aux prepare_vg 2
+aux prepare_vg 2 20000
 
-lvcreate -l2 -T $vg/pool
+lvcreate -l1 -T $vg/pool
 # Caching of thin-pool's dataLV
 lvcreate -H -L10 $vg/pool
 
-# Resize is unsupported
-not lvextend -l+2 $vg/pool 2>&1 | tee out
+lvextend -l+2 $vg/pool
 
-grep "Unable to resize" out
+check lv_first_seg_field $vg/pool seg_size_pe "3"
+
+lvextend -L10G $vg/pool
+
+# Check data are resized and its metadata are matching data size
+check lv_field $vg/pool size		 "10.00g"
+check lv_field $vg/pool_tdata size	 "10.00g"
+check lv_field $vg/pool_tdata_corig size "10.00g"
+check lv_field $vg/pool_tmeta size "10.00m"
 
 vgremove -ff $vg
