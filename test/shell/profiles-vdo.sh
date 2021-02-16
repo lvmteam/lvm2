fbaf5a32bb3465d121f60db5a77df2e86b0bf49b
Zdenek Kabelac <zkabelac@redhat.com>
2021-02-16 21:30:40 +0100

    tests: lvconvert vdo profile support

--- test/shell/profiles-vdo.sh
+++ test/shell/profiles-vdo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (C) 2018 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2018-2021 Red Hat, Inc. All rights reserved.
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
@@ -10,7 +10,7 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-# Exercise obtaining cache parameter from various sources
+# Exercise obtaining vdo parameters from various sources
 # Either commmand line or metadata profile or implicit default...
 
 
@@ -46,5 +46,9 @@ lvcreate --vdo -L4G --metadataprofile "$PFILE" $vg/vdopool
 lvdisplay -m $vg/vdopool | tee out
 grep "Compression.*no" out
 lvremove -f $vg
+
+lvcreate -L4G --name vdopool $vg
+lvconvert --yes --type vdo-pool --metadataprofile "$PFILE" $vg/vdopool
+check lv_field $vg/vdopool vdo_compression ""
 
 vgremove -ff $vg
