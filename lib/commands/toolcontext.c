d37b19aaaab0c28f4d1e4ae0690c73c0031a027b
Zdenek Kabelac <zkabelac@redhat.com>
2021-03-18 23:08:08 +0100

    gcc: ensure buffer ends with 0

--- lib/commands/toolcontext.c
+++ lib/commands/toolcontext.c
@@ -230,7 +230,7 @@ static void _get_sysfs_dir(struct cmd_context *cmd, char *buf, size_t buf_size)
 		return;
 	}
 
-	strncpy(buf, sys_mnt, buf_size);
+	(void) dm_strncpy(buf, sys_mnt, buf_size);
 }
 
 static uint32_t _parse_debug_fields(struct cmd_context *cmd, int cfg, const char *cfgname)
