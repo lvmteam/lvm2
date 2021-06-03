8c7b2df41fdddcd1b1c504522ab79300882eb72f
Leo Yan <leo.yan@linaro.org>
2021-06-03 17:59:16 +0800

    tests: Support idm failure injection
    When the drive failure occurs, the IDM lock manager and lvmlockd should
    handle this case properly.  E.g. when the IDM lock manager detects the
    lease renewal failure caused by I/O errors, it should invoke the kill
    path which is predefined by lvmlockd, so that the kill path program
    (like lvmlockctl) can send requests to lvmlockd to stop and drop lock
    for the relevant VG/LVs.
    
    To verify the failure handling flow, this patch introduces an idm
    failure injection program, it can input the "percentage" for drive
    failures so that can emulate different failure cases.
    
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

---
+++ test/lib/idm_inject_failure.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2020-2021 Seagate Ltd.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License v.2.1.
+ */
+
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/inotify.h>
+#include <uuid/uuid.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <ilm.h>
+
+int main(int argc, char *argv[])
+{
+	int pecent = atoi(argv[1]);
+	int ret, s;
+
+	ret = ilm_connect(&s);
+	if (ret == 0) {
+		printf("ilm_connect: SUCCESS\n");
+	} else {
+		printf("ilm_connect: FAIL\n");
+		exit(-1);
+	}
+
+	ret = ilm_inject_fault(s, pecent);
+	if (ret == 0) {
+		printf("ilm_inject_fault (100): SUCCESS\n");
+	} else {
+		printf("ilm_inject_fault (100): FAIL\n");
+		exit(-1);
+	}
+
+	ret = ilm_disconnect(s);
+	if (ret == 0) {
+		printf("ilm_disconnect: SUCCESS\n");
+	} else {
+		printf("ilm_disconnect: FAIL\n");
+		exit(-1);
+	}
+
+	return 0;
+}
