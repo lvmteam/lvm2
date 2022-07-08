e2e31d9acf1b96ab741c22dc0a2fefd672996d3a
Zdenek Kabelac <zkabelac@redhat.com>
2022-07-08 23:38:34 +0200

    vdo: enhance lvcreate validation
    When creating VDO pool based of  % values, lvm2 is now more clever
    and avoids to create 'unsupportable' sizes of physical backend
    volumes as 16TiB is maximum size supported by VDO target
    (and also limited by maximum supportable slabs (8192) based on slab
    size.
    
    If the requested virtual size is approaching max supported size 4PiB,
    switch header size to 0.

--- lib/metadata/metadata-exported.h
+++ lib/metadata/metadata-exported.h
@@ -1364,6 +1364,8 @@ const char *get_vdo_operating_mode_name(enum dm_vdo_operating_mode mode);
 const char *get_vdo_write_policy_name(enum dm_vdo_write_policy policy);
 uint64_t get_vdo_pool_virtual_size(const struct lv_segment *vdo_pool_seg);
 int update_vdo_pool_virtual_size(struct lv_segment *vdo_pool_seg);
+uint32_t get_vdo_pool_max_extents(const struct dm_vdo_target_params *vtp,
+				  uint32_t extent_size);
 int parse_vdo_pool_status(struct dm_pool *mem, const struct logical_volume *vdo_pool_lv,
 			  const char *params, const struct dm_info *dminfo,
 			  struct lv_status_vdo *status);
