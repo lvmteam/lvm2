1605d39b93d2376824911951755c3b78fd2c805a
Zdenek Kabelac <zkabelac@redhat.com>
2023-10-10 22:07:30 +0200

    tests: comment about version checking to aux.sh
    If there would be futher version checking needed,
    make a notice there are now two -V formats.

--- test/lib/aux.sh
+++ test/lib/aux.sh
@@ -1732,6 +1732,7 @@ thin_pool_error_works_32() {
 }
 
 thin_restore_needs_more_volumes() {
+	# Note: new version prints thin_restore first
 	case $("$LVM_TEST_THIN_RESTORE_CMD" -V) in
 		# With older version of thin-tool we got slightly more compact metadata
 		0.[0-6]*|0.7.0*) return 0 ;;
