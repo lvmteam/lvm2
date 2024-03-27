69f9fcfd20ffdd8c3af98a33c7eb4bdf0ee3caf7
Zdenek Kabelac <zkabelac@redhat.com>
2024-03-27 01:25:45 +0100

    tests: skip some mirror tests for polld and lockd
    Leave just lvconvert-mirror-basic-1 test
    for polld and lockd testing.

--- test/shell/lvconvert-mirror-basic-0.sh
+++ test/shell/lvconvert-mirror-basic-0.sh
@@ -10,6 +10,9 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+SKIP_WITH_LVMLOCKD=1
+SKIP_WITH_LVMPOLLD=1
+
 . ./shell/lvconvert-mirror-basic.sh
 
 test_many 0
