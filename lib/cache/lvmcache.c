fb80ed314394a3e644fa2ad820441e99023689e1
Zdenek Kabelac <zkabelac@redhat.com>
2024-03-29 15:42:38 +0100

    cleanup: remove lvmcache_has_bad_metadata

--- lib/cache/lvmcache.c
+++ lib/cache/lvmcache.c
@@ -191,21 +191,6 @@ static void _destroy_device_list(struct dm_list *head)
 	dm_list_init(head);
 }
 
-bool lvmcache_has_bad_metadata(struct device *dev)
-{
-	struct lvmcache_info *info;
-
-	if (!(info = lvmcache_info_from_pvid(dev->pvid, dev, 0))) {
-		/* shouldn't happen */
-		log_error("No lvmcache info for checking bad metadata on %s", dev_name(dev));
-		return false;
-	}
-
-	if (info->mda1_bad || info->mda2_bad)
-		return true;
-	return false;
-}
-
 void lvmcache_save_bad_mda(struct lvmcache_info *info, struct metadata_area *mda)
 {
 	if (mda->mda_num == 1)
