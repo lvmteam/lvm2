2a1e200f7af3d4c52afbb4627c8cf87f98baa71e
Zdenek Kabelac <zkabelac@redhat.com>
2024-05-03 21:50:38 +0200

    libdm: fix condition
    When we switch supported_reserved_types_with_range to const
    gcc repots this problem:
    
     warning: ‘and’ of mutually exclusive equal-tests is always 0
     !(iter->type & supported_reserved_types_with_range))) {
    
    It's not clear from the history what was the actual intention of this
    internal error test, let's assume the check wanted to make sure
    that when DM_REPORT_FIELD_RESERVED_VALUE_RANGE is set,
    some other fields from supported_reserved_types_with_range set
    are also selected.

--- libdm/libdm-report.c
+++ libdm/libdm-report.c
@@ -2587,7 +2587,8 @@ static int _check_reserved_values_supported(const struct dm_report_field_type fi
 		if (iter->type & DM_REPORT_FIELD_TYPE_MASK) {
 			if (!(iter->type & supported_reserved_types) ||
 			    ((iter->type & DM_REPORT_FIELD_RESERVED_VALUE_RANGE) &&
-			     !(iter->type & supported_reserved_types_with_range))) {
+			     !(iter->type & (supported_reserved_types_with_range &
+					     ~DM_REPORT_FIELD_RESERVED_VALUE_RANGE)))) {
 				log_error(INTERNAL_ERROR "_check_reserved_values_supported: "
 					  "global reserved value for type 0x%x not supported",
 					   iter->type);
@@ -2598,7 +2599,8 @@ static int _check_reserved_values_supported(const struct dm_report_field_type fi
 			field = &fields[field_res->field_num];
 			if (!(field->flags & supported_reserved_types) ||
 			    ((iter->type & DM_REPORT_FIELD_RESERVED_VALUE_RANGE) &&
-			     !(iter->type & supported_reserved_types_with_range))) {
+			     !(iter->type & (supported_reserved_types_with_range &
+					     ~DM_REPORT_FIELD_RESERVED_VALUE_RANGE)))) {
 				log_error(INTERNAL_ERROR "_check_reserved_values_supported: "
 					  "field-specific reserved value of type 0x%x for "
 					  "field %s not supported",
