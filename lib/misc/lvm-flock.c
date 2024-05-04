db40a2e33c5cb7c58548310a70b9b91f8cac1298
Zdenek Kabelac <zkabelac@redhat.com>
2024-05-04 22:00:51 +0200

    cov: replace strcat with safe snprintf

--- lib/misc/lvm-flock.c
+++ lib/misc/lvm-flock.c
@@ -159,10 +159,9 @@ static int _do_flock(const char *file, int *fd, int operation, uint32_t nonblock
 static int _do_write_priority_flock(const char *file, int *fd, int operation, uint32_t nonblock)
 {
 	int r, fd_aux = -1;
-	char *file_aux = alloca(strlen(file) + sizeof(AUX_LOCK_SUFFIX));
+	char file_aux[PATH_MAX];
 
-	strcpy(file_aux, file);
-	strcat(file_aux, AUX_LOCK_SUFFIX);
+	snprintf(file_aux, sizeof(file_aux), "%s%s", file, AUX_LOCK_SUFFIX);
 
 	if ((r = _do_flock(file_aux, &fd_aux, LOCK_EX, nonblock))) {
 		if (operation == LOCK_EX) {
