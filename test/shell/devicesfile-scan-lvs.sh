f10de12adeee52a1aa75a851ff6d9d484d7ad471
Zdenek Kabelac <zkabelac@redhat.com>
2024-05-27 21:29:58 +0200

    tests: quoted DM_DEV_DIR

--- test/shell/devicesfile-scan-lvs.sh
+++ test/shell/devicesfile-scan-lvs.sh
@@ -33,26 +33,26 @@ touch "$DF"
 vgcreate $vg1 "$dev1"
 lvcreate -n $lv1 -L 8M $vg1
 lvcreate -n $lv2 -L 8M $vg1
-pvcreate $DM_DEV_DIR/$vg1/$lv1
-pvcreate $DM_DEV_DIR/$vg1/$lv2
-vgcreate $vg2 $DM_DEV_DIR/$vg1/$lv1 $DM_DEV_DIR/$vg1/$lv2
+pvcreate "$DM_DEV_DIR/$vg1/$lv1"
+pvcreate "$DM_DEV_DIR/$vg1/$lv2"
+vgcreate $vg2 "$DM_DEV_DIR/$vg1/$lv1" "$DM_DEV_DIR/$vg1/$lv2"
 
-pvs $DM_DEV_DIR/$vg1/$lv1
-pvs $DM_DEV_DIR/$vg1/$lv2
+pvs "$DM_DEV_DIR/$vg1/$lv1"
+pvs "$DM_DEV_DIR/$vg1/$lv2"
 
 grep "$dev1" "$DF"
-grep $DM_DEV_DIR/$vg1/$lv1 "$DF" | tee out1
+grep "$DM_DEV_DIR/$vg1/$lv1" "$DF" | tee out1
 grep "IDTYPE=lvmlv_uuid" out1
 grep "IDNAME=LVM-" out1
-grep $DM_DEV_DIR/$vg1/$lv2 "$DF" | tee out2
+grep "$DM_DEV_DIR/$vg1/$lv2" "$DF" | tee out2
 grep "IDTYPE=lvmlv_uuid" out2
 grep "IDNAME=LVM-" out2
 
 lvremove -y $vg1/$lv1
-not grep $DM_DEV_DIR/$vg1/$lv1 "$DF"
+not grep "$DM_DEV_DIR/$vg1/$lv1" "$DF"
 
 lvremove -y $vg1/$lv2
-not grep $DM_DEV_DIR/$vg1/$lv2 "$DF"
+not grep "$DM_DEV_DIR/$vg1/$lv2" "$DF"
 
 grep "$dev1" "$DF"
 not grep "IDTYPE=lvmlv_uuid" "$DF"
