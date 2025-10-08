d991c7c5c2b4005aacb8b97a065202fadee2f057
Zdenek Kabelac <zkabelac@redhat.com>
2025-10-08 13:40:25 +0200

    coverity: improve static analysis models
    Improvements to Coverity modeling file to enable better
    static analysis of LVM2 code.
    
    1. Improve first_seg()/last_seg()/find_config_tree_str()/
       find_config_tree_str_allow_empty() - now models that these never
       return NULL.
    
    New Models Added:
    2. dm_pool_alloc/zalloc/strdup/strndup - Core allocation functions
       - Models that allocation can fail (returns NULL)
       - Tracks uninitialized vs zeroed memory
       - Detects negative size arguments
       - Properly models string null-termination
    
    3. dm_malloc/zalloc/free wrappers - Standard allocation wrappers
       - Resource tracking for leak detection
       - Allocation failure modeling
    
    4. dm_list operations - List manipulation functions
       - Models that operations don't fail for valid inputs
       - Catches NULL pointer programming errors

--- coverity/coverity_model.c
+++ coverity/coverity_model.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2015-2025 Red Hat, Inc. All rights reserved.
  *
  * This file is part of LVM2.
  *
@@ -28,128 +28,288 @@
  * cov-format-errors --dir cov  --html-output html
  */
 
+/* Define NULL for Coverity modeling */
+#define NULL ((void *)0)
+
+/* Forward declarations */
 struct lv_segment;
 struct logical_volume;
+struct segment_type;
 struct cmd_context;
 struct profile;
+struct dm_pool;
+struct dm_list;
 
+/*
+ * These functions never return NULL for valid LVs with segments
+ */
 struct lv_segment *first_seg(const struct logical_volume *lv)
 {
-	return ((struct lv_segment **)lv)[0];
+	struct lv_segment *seg;
+
+	/* Model: assume lv is valid and has at least one segment */
+	if (lv) {
+		__coverity_read_pointee__(lv);
+		return seg;
+	}
+
+	__coverity_panic__(); /* Should never happen in valid code */
 }
 
 struct lv_segment *last_seg(const struct logical_volume *lv)
 {
-	return ((struct lv_segment **)lv)[0];
-}
+	struct lv_segment *seg;
 
-const char *find_config_tree_str(struct cmd_context *cmd, int id, struct profile *profile)
-{
-	return "STRING";
-}
-
-/*
-struct logical_volume *origin_from_cow(const struct logical_volume *lv)
-{
-	if (lv)
-		return lv;
+	if (lv) {
+		__coverity_read_pointee__(lv);
+		return seg;
+	}
 
 	__coverity_panic__();
 }
-*/
-
-/* simple_memccpy() from glibc */
-void *memccpy(void *dest, const void *src, int c, unsigned long n)
-{
-	const char *s = src;
-	char *d = dest;
-
-	while (n-- > 0)
-		if ((*d++ = *s++) == (char) c)
-			return d;
-
-	return 0;
-}
-
-/*
- * 2 lines below needs to be placed in coverity/config/user_nodefs.h
- * Not sure about any other way.
- * Without them, coverity shows warning since x86 system header files
- * are using inline assembly to reset fdset
- */
-//#nodef FD_ZERO model_FD_ZERO
-//void model_FD_ZERO(void *fdset);
-
-void model_FD_ZERO(void *fdset)
-{
-	unsigned i;
-
-	for (i = 0; i < 1024 / 8 / sizeof(long); ++i)
-		((long*)fdset)[i] = 0;
-}
-
-
-/* Resent Coverity reports quite weird errors... */
-int *__errno_location(void)
-{
-	static int _i = 0;
-	return &_i;
-}
-
-const unsigned short **__ctype_b_loc (void)
-{
-	static const unsigned short *_a[1] = { 0 };
-	return _a;
-}
-
-
-
-/*
- * Added extra pointer check to not need these models,
- * for now just keep then in file
- */
-
-/*
-struct cmd_context;
-struct profile;
 
 const char *find_config_tree_str(struct cmd_context *cmd, int id, struct profile *profile)
 {
-        return "text";
+	char *str;
+
+	__coverity_read_pointee__(cmd);
+
+	if (str)
+		return str;
+
+	__coverity_panic__();
 }
 
 const char *find_config_tree_str_allow_empty(struct cmd_context *cmd, int id, struct profile *profile)
 {
-        return "text";
-}
-*/
+	char *str;
 
-/*
- * Until fixed coverity case# 00531860:
- *   A FORWARD_NULL false positive on a recursive function call
- *
- * model also these functions:
- */
-/*
-const struct dm_config_node;
-const struct dm_config_node *find_config_tree_array(struct cmd_context *cmd, int id, struct profile *profile)
+	__coverity_read_pointee__(cmd);
+
+	if (str)
+		return str;
+
+	__coverity_panic__();
+}
+
+struct segment_type *init_unknown_segtype(struct cmd_context *cmd, const char *name)
 {
-	const struct dm_config_node *cn;
+	struct segment_type *seg_type;
 
-	return cn;
+	__coverity_read_pointee__(cmd);
+	__coverity_read_pointee__(name);
+
+	if (seg_type)
+		return seg_type;
+
+	__coverity_panic__();
 }
 
-const struct dm_config_node *find_config_tree_node(struct cmd_context *cmd, int id, struct profile *profile)
+/* simple_memccpy() from glibc */
+void *memccpy(void *dest, const void *src, int c, unsigned long n)
 {
-	const struct dm_config_node *cn;
+	int success;
 
-	return cn;
+	__coverity_negative_sink__(n);
+	__coverity_negative_sink__(c);
+	__coverity_tainted_data_transitive__(dest, src);
+	__coverity_write_buffer_bytes__(dest, n);
+
+	if (!success)
+		return NULL;
+
+	return dest;
 }
 
-int find_config_tree_bool(struct cmd_context *cmd, int id, struct profile *profile)
+/* dm_pool_alloc - can return NULL on allocation failure */
+void *dm_pool_alloc(struct dm_pool *p, unsigned long s)
 {
-	int b;
+	void *ptr;
+	int success;
 
-	return b;
+	if (!p) {
+		__coverity_panic__();
+	}
+
+	__coverity_negative_sink__(s);  /* Catch negative sizes */
+
+	if (!success)
+		return NULL;
+
+	ptr = __coverity_alloc__(s);
+
+	/* Mark as escaped - pool memory doesn't need individual free */
+	__coverity_escape__(ptr);
+
+	return ptr;
 }
-*/
+
+/* dm_pool_zalloc - allocates and zeros */
+void *dm_pool_zalloc(struct dm_pool *p, unsigned long s)
+{
+	void *ptr;
+	int success;
+
+	if (!p) {
+		__coverity_panic__();
+	}
+
+	__coverity_negative_sink__(s);  /* Catch negative sizes */
+
+	if (!success)
+		return NULL;
+
+	ptr = __coverity_alloc__(s);
+	__coverity_writeall0__(ptr);  /* Memory is zeroed */
+
+	/* Mark as escaped - pool memory doesn't need individual free */
+	__coverity_escape__(ptr);
+
+	return ptr;
+}
+
+/* dm_pool_strdup - duplicates string in pool */
+char *dm_pool_strdup(struct dm_pool *p, const char *str)
+{
+	char *ptr;
+	unsigned long size;
+	int success;
+
+	if (!p || !str) {
+		__coverity_panic__();
+	}
+
+	__coverity_string_null_sink__(str);  /* str must be null-terminated */
+	__coverity_string_size_sink__(str);  /* Coverity tracks the size */
+
+	if (!success)
+		return NULL;
+
+	/* Allocate symbolic size - Coverity will track this appropriately */
+	ptr = __coverity_alloc__(size);
+	__coverity_tainted_data_transitive__(ptr, str);
+
+	/* Mark as escaped - pool memory doesn't need individual free */
+	__coverity_escape__(ptr);
+
+	return ptr;
+}
+
+/* dm_pool_strndup - duplicates up to n characters */
+char *dm_pool_strndup(struct dm_pool *p, const char *str, unsigned long n)
+{
+	char *ptr;
+	int success;
+
+	if (!p) {
+		__coverity_panic__();
+	}
+	__coverity_string_size_source__(str);
+	__coverity_negative_sink__(n);
+
+	if (!success)
+		return NULL;
+
+	ptr = __coverity_alloc__(n + 1);
+
+	__coverity_tainted_data_transitive__(ptr, str);
+	__coverity_string_null_copy__(ptr, str, n);
+
+	/* Mark as escaped - pool memory doesn't need individual free */
+	__coverity_escape__(ptr);
+
+	return ptr;
+}
+
+/* dm_pool_free - frees memory back to the pool */
+void dm_pool_free(struct dm_pool *p, void *ptr)
+{
+	if (!p || !ptr) {
+		__coverity_panic__();
+	}
+}
+
+void dm_pool_empty(struct dm_pool *p)
+{
+	if (!p) {
+		__coverity_panic__();
+	}
+}
+
+/* dm_malloc - standard malloc wrapper */
+void *dm_malloc_wrapper(unsigned long s, const char *file, int line)
+{
+	void *ptr;
+	int success;
+
+	__coverity_negative_sink__(s);
+
+	if (!success)
+		return NULL;
+
+	ptr = __coverity_alloc__(s);
+
+	__coverity_mark_as_afm_allocated__(ptr, AFM_free);
+
+	return ptr;
+}
+
+/* dm_zalloc - standard calloc wrapper */
+void *dm_zalloc_wrapper(unsigned long s, const char *file, int line)
+{
+	void *ptr;
+	int success;
+
+	__coverity_negative_sink__(s);
+
+	if (!success)
+		return NULL;
+
+	ptr = __coverity_alloc__(s);
+
+	__coverity_mark_as_afm_allocated__(ptr, AFM_free);
+	__coverity_writeall0__(ptr);
+
+	return ptr;
+}
+
+/* dm_free - standard free wrapper */
+void dm_free_wrapper(void *ptr, const char *file, int line)
+{
+	if (ptr) {
+		__coverity_free__(ptr);
+	}
+}
+
+
+/* dm_list_init - initializes a list head */
+void dm_list_init(struct dm_list *head)
+{
+	if (head) {
+		__coverity_writeall__(head);
+	}
+}
+
+/* dm_list_empty - checks if list is empty (never fails) */
+int dm_list_empty(const struct dm_list *head)
+{
+	int is_empty;
+
+	if (head)
+		return is_empty;
+
+	__coverity_panic__();
+}
+
+/* dm_list_add - adds to list (never fails) */
+void dm_list_add(struct dm_list *head, struct dm_list *elem)
+{
+	if (!head || !elem) {
+		__coverity_panic__();
+	}
+
+	/* Modification happens, but no failure */
+	__coverity_writeall__(head);
+	__coverity_writeall__(elem);
+	__coverity_escape__(elem);
+}
