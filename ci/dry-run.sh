5d59d223353bc95650c065a4d84b8a1f24bde508
Marian Csontos <mcsontos@redhat.com>
2026-03-02 20:15:48 +0100

    CI: Add dry-run option to CI

---
+++ ci/dry-run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+die() {
+	echo "ERROR: $*" >&2
+	exit 1
+}
+
+hash() {
+	git log -1 --pretty="format:%h" "$@"
+}
+
+# TODO: This just compares HEAD with main. Do we care about other branches?
+HEAD="${1:-"HEAD"}"
+DESCRIBE="$(git describe "$HEAD")"
+HASH="$(hash "$HEAD")"
+TARGET=${2:-"main"}
+
+answ=0
+
+if git diff --name-only | grep -v '^\(\.gitlab-ci.yaml\|ci/\|WHATS_NEW\|VERSION\|man/\|doc/\|README\|TESTING\|COPYING\|INSTALL\|\.gitignore\|coverity\|ikiwiki.setup/\|nix/\|po/\)'; then
+	echo "INFO: Changed files, running CI" >&2
+	exit 1
+else
+	echo "INFO: Dry run, no significant files changed " >&2
+	exit 0
+fi
