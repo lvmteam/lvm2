5ddd92c345556d5fe16fe003a740049e4b144871
Marian Csontos <mcsontos@redhat.com>
2026-03-04 16:11:55 +0100

    CI: fix the git reference in dry-run.sh

--- ci/dry-run.sh
+++ ci/dry-run.sh
@@ -13,13 +13,14 @@ hash() {
 HEAD="${1:-"HEAD"}"
 DESCRIBE="$(git describe "$HEAD")"
 HASH="$(hash "$HEAD")"
-TARGET=${2:-"main"}
+TARGET=${2:-"origin/main"}
 
 answ=0
 
 echo "Checking files changed in the MR for signigicant changes:"
 git diff --name-only "${HEAD}" "^${TARGET}"
 echo "-----"
+
 if git diff --name-only "${HEAD}" "^${TARGET}" | grep -v '^\(\.gitlab-ci.yaml\|ci/\|WHATS_NEW\|VERSION\|man/\|doc/\|README\|TESTING\|COPYING\|INSTALL\|\.gitignore\|coverity\|ikiwiki.setup/\|nix/\|po/\)'; then
 	echo "INFO: Changed files, running CI" >&2
 	exit 1
